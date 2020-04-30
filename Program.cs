ffcdb5491fdbccf87cb3b8913f07883fe3e998e6
Nougator <37145431+nougator@users.noreply.github.com>
2020-04-30 23:18:56 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PocketCraft_MP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
