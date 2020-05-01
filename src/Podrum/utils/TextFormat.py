8b9e31d4a349daf459d934fdfac4d4db8a14cfc5
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-01 15:40:53 +0300

    Update TextFormat.py

--- src/Podrum/utils/TextFormat.py
+++ src/Podrum/utils/TextFormat.py
@@ -1,1 +1,24 @@
+from re import M
+from .checkOS import getOS
 
+class TextFormat:
+    def __init__(self):
+        if getOS() == 'windows':
+            self.WHITE = '\33[37m'
+            self.RED = '\33[31m'
+            self.GREEN = '\33[32m'
+            self.YELLOW = '\33[33m'
+            self.BLUE = '\33[34m'
+            self.VIOLET = '\33[35m'
+            self.BEIGE = '\33[36m'
+            self.BLACK = '\33[30m'
+        elif getOS() == 'osx' or getOS() == 'linux':
+            # TODO: Add UNIX colors
+            self.WHITE = ''
+            self.RED = ''
+            self.GREEN = ''
+            self.YELLOW = ''
+            self.BLUE = ''
+            self.VIOLET = ''
+            self.BEIGE = ''
+            self.BLACK = ''
