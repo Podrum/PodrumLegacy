918e16ede4b407cc446955e8c92700c58c0d977a
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-01 22:35:51 +0300

    Update Server.py

--- src/Podrum/Server.py
+++ src/Podrum/Server.py
@@ -13,6 +13,8 @@
 
 from threading import Thread
 import time
+from pyraklib.server import PyRakLibServer
+from pyraklib.server import ServerHandler
 
 from .utils import logger, fs
 
@@ -21,7 +23,12 @@ class Server(Thread):
     def __init__(self, path):
         super().__init__()
         fs.checkAllFiles(path)
+        port = 19132
         logger.log('info', 'Starting server...')
+        logger.log('info', '____           _\n|  _ \ ___   __| |_ __ _   _ _ __ ___ \n | |_) / _ \ / _` | '__| | | | '_ ` _ \ \n |  __/ (_) | (_| | |  | |_| | | | | | | \n |_|   \___/ \__,_|_|   \__,_|_| |_| |_|')
+        logger.log('info', 'Podrum is licensed under the GPLv3 license')
+        server = PyRakLibServer(port)
+        logger.log('info', 'Starting server on *:' + str(port)
 
         ticking = True
         while ticking:
