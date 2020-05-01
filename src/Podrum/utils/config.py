a6b8bbad9073a5dedd0fec8630deb939f8ea823b
Nougator <37145431+nougator@users.noreply.github.com>
2020-05-01 16:35:34 +0200

    Moved function in config to fs

--- src/Podrum/utils/config.py
+++ src/Podrum/utils/config.py
@@ -1,32 +1,3 @@
 import os
 
 from ..wizard.wizard import wizard
-
-
-class Config:
-    def read(self):
-        pass
-
-    def write(self):
-        pass
-
-    # Name include extension
-    def createFiles(self, path, name):
-        open(f'{path}/{name}', 'w+')
-
-    def createDir(self, path, name):
-        os.mkdir(f'{path}/{name}')
-
-    def checkForFile(self, type_, path, name):
-        if type_.lower() == 'file' or 'f':
-            return os.path.isfile(f'{path}/{name}')
-        elif type_.lower() == 'directory' or 'dir':
-            return os.path.isdir(f'{path}/{name}')
-
-    def checkAllFiles(self, path):
-        if not self.checkForFile('f', path, 'server.json'):
-            self.createFiles(path, 'server.json')
-        elif not self.checkForFile('dir', path, 'plugins'):
-            self.createDir(path, 'plugins')
-        elif not self.checkForFile('dir', path, 'worlds'):
-            self.createDir(path, 'worlds')
---
+++ src/Podrum/utils/fs.py
@@ -0,0 +1,30 @@
+import os
+
+
+class fs:
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
+    # Name include extension
+    def createFiles(self, path, name):
+        open(f'{path}/{name}', 'w+')
+
+    def createDir(self, path, name):
+        os.mkdir(f'{path}/{name}')
+
+    def checkForFile(self, type_, path, name):
+        if type_.lower() == 'file' or 'f':
+            return os.path.isfile(f'{path}/{name}')
+        elif type_.lower() == 'directory' or 'dir':
+            return os.path.isdir(f'{path}/{name}')
+
+    def checkAllFiles(self, path):
+        if not self.checkForFile('f', path, 'server.json'):
+            self.createFiles(path, 'server.json')
+        elif not self.checkForFile('dir', path, 'plugins'):
+            self.createDir(path, 'plugins')
+        elif not self.checkForFile('dir', path, 'worlds'):
+            self.createDir(path, 'worlds')
