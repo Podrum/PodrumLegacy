f999282e0b56487fb8f70c4c32fcda3fdb9aa4dc
Nougator <37145431+nougator@users.noreply.github.com>
2020-05-01 21:41:08 +0200

    Update Server.py

--- src/Podrum/Server.py
+++ src/Podrum/Server.py
@@ -13,8 +13,8 @@
 
 from threading import Thread
 import time
-from pyraklib.server import PyRakLibServer
-from pyraklib.server import ServerHandler
+from ..pyraklib.server import PyRakLibServer
+from ..pyraklib.server import ServerHandler
 
 from .utils import logger, fs
 
@@ -25,7 +25,8 @@ class Server(Thread):
         fs.checkAllFiles(path)
         port = 19132
         logger.log('info', 'Starting server...')
-        logger.log('info', '____           _\n|  _ \ ___   __| |_ __ _   _ _ __ ___ \n | |_) / _ \ / _` | '__| | | | '_ ` _ \ \n |  __/ (_) | (_| | |  | |_| | | | | | | \n |_|   \___/ \__,_|_|   \__,_|_| |_| |_|')
+        logger.log('info', '____           _\n|  _ \ ___   __| |_ __ _   _ _ __ ___ \n | |_) / _ \ / _` | \'__| | | | '
+                           '\'_ ` _ \ \n |  __/ (_) | (_| | |  | |_| | | | | | | \n |_|   \___/ \__,_|_|   \__,_|_| |_| |_|')
         logger.log('info', 'Podrum is licensed under the GPLv3 license')
         server = PyRakLibServer(port)
         logger.log('info', 'Starting server on *:' + str(port))
