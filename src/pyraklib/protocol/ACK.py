e4816184cba925c741c346d21c7f83f4d14c58dd
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 10:52:59 +0300

    Add files via upload

--- src/pyraklib/protocol/ACK.py
+++ src/pyraklib/protocol/ACK.py
@@ -19,6 +19,7 @@ PyRakLib networking library.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+from pyraklib.protocol.AcknowledgePacket import AcknowledgePacket
 
 class ACK(AcknowledgePacket):
 
--- src/pyraklib/protocol/ADVERTISE_SYSTEM.py
+++ src/pyraklib/protocol/ADVERTISE_SYSTEM.py
@@ -19,6 +19,8 @@ PyRakLib networking library.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+from pyraklib.protocol.UNCONNECTED_PONG import UNCONNECTED_PONG
+
 
 class ADVERTISE_SYSTEM(UNCONNECTED_PONG):
     PID = 0x1D
