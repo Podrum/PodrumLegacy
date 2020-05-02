f7c34932eecd944a9de6756f82b87be67e8bae93
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 13:33:45 +0300

    Update ipAddr.py

--- src/Podrum/network/ipAddr.py
+++ src/Podrum/network/ipAddr.py
@@ -1,5 +1,11 @@
-import netifaces
+import socket
+import fcntl
+import struct
 
-def getPrivateIpAddr():
-    netifaces.ifaddresses('eth0')
-    return netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+def getPrivateIpAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
