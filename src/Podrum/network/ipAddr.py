32b5fb8489f0e57ed581d0ec28dca3353a3dcd29
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 13:23:07 +0300

    Update ipAddr.py

--- src/Podrum/network/ipAddr.py
+++ src/Podrum/network/ipAddr.py
@@ -1,6 +1,6 @@
 import netifaces
 
-def getPublicIpAddr():
+def getPrivateIpAddr():
     netifaces.ifaddresses('eth0')
     ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
     printf ip
