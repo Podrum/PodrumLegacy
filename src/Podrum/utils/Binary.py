b5e5a415a480719e1bb0706be4e35c2d53bfe646
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 17:44:19 +0300

    Update Binary.py

--- src/Podrum/utils/Binary.py
+++ src/Podrum/utils/Binary.py
@@ -13,7 +13,7 @@ class Binary
     @staticmethod
     def checkLength(str, expect):
         len = self.strlen(str)
-        assert (len == expect), 'Expected ' + expect + ' bytes, got' + len
+        assert (len == expect), f'Expected {str(expect)} bytes got {str(len)}'
 
     @staticmethod
     def readTriad(str):
