2d1d415d112ca5958bf549ef5ee950b788466832
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 15:50:31 +0300

    Update ipAddr.py

--- src/Podrum/network/ipAddr.py
+++ src/Podrum/network/ipAddr.py
@@ -1,6 +1,6 @@
 import socket
 
-def getPublicIpAddr():
+def getPrivateIpAddr():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
