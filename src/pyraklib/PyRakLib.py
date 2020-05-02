e561775d29560636ed8fcb771cde284d94f1ae44
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-02 11:21:35 +0300

    Update PyRakLib.py

--- src/pyraklib/PyRakLib.py
+++ src/pyraklib/PyRakLib.py
@@ -25,8 +25,8 @@ class PyRakLib:
     __metaclass__ = ABCMeta
 
     LIBRARY_VERSION = "1.0.14b1"
-    VERSION = "0.8.0"
-    PROTOCOL = 6
+    VERSION = "0.7.0"
+    PROTOCOL = 5
     MAGIC = bytearray.fromhex(" 00 ff ff 00 fe fe fe fe fd fd fd fd 12 34 56 78")
 
     PRIORITY_NORMAL = 0
