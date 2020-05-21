e8e2433fa2cd3e7e6ffedb102f5bc389cca564a5
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-21 19:37:59 +0300

    Update Utils.py

--- src/Podrum/utils/Utils.py
+++ src/Podrum/utils/Utils.py
@@ -1,1 +1,23 @@
+"""
+*  ____           _                      
+* |  _ \ ___   __| |_ __ _   _ _ __ ___  
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \ 
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
+import sys
 
+class Utils:
+
+    def getOS():
+        if sys.platform == 'linux' or sys.platform == 'linux2':
+            return 'linux'
+        elif sys.platform == 'darwin':
+            return 'osx'
+        elif sys.platform == 'win32' or sys.platform == 'win64':
+            return 'windows'
