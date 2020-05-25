266090bfb7430c59becf6a2550ac4c955eb10f44
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-05-25 12:51:46 -0500

    Moved command execution (from here to Server.py)

--- start.py
+++ start.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-from src.Podrum.Server import Server, command
+from src.Podrum.Server import Server
 from os import getcwd
 from threading import Thread
 
 serverThread = Thread(target=Server, args=(getcwd(),))
 serverThread.start()
-
-while True:
-    cmd = input('> ')
-    command(cmd, True)
-    cmd = None
