997bcd114ecafec80774d03bb728fd3ab771d72a
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-26 16:58:07 +0300

    Update and rename src/Podrum/Podrum.py to Podrum.py

--- Podrum.py
+++ Podrum.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.insert(0, '..') 
-
 from os import getcwd
 from threading import Thread
-from Podrum.Server import Server
+from src.Podrum.Server import Server
 
 serverThread = Thread(target=Server, args=(getcwd(),))
 serverThread.start()
