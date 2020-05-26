d3b32ec346bd3bd73c04555d151fea8989719fb0
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-26 16:37:46 +0300

    Update Podrum.py

--- src/Podrum/Podrum.py
+++ src/Podrum/Podrum.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+
 from os import getcwd
 from threading import Thread
-from Server import Server
+from Podrum.Server import Server
 
 serverThread = Thread(target=Server, args=(getcwd(),))
 serverThread.start()
