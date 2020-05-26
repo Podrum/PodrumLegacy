4721e577984834144bc954d47d9b8b38f1998107
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-26 16:29:09 +0300

    Update Podrum.py

--- src/Podrum/Podrum.py
+++ src/Podrum/Podrum.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
-import os
-os.chdir("..")
-
 from os import getcwd
 from threading import Thread
-from .Server import Server
+from Server import Server
 
 serverThread = Thread(target=Server, args=(getcwd(),))
 serverThread.start()
