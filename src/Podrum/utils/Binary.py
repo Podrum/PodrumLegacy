776c239b4d49f05634a2ce47a509b70eef14513f
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-29 09:23:38 +0300

    Update Binary.py

--- src/Podrum/utils/Binary.py
+++ src/Podrum/utils/Binary.py
@@ -15,42 +15,33 @@ import sys
 
 class Binary:
 
-    @staticmethod
     def strlen(x):
         return len(x)
     
-    @staticmethod
     def checkLength(string, expect):
         len = self.strlen(string)
         assert (len == expect), 'Expected ' + str(expect) + 'bytes, got ' + str(len)
 
-    @staticmethod
     def readTriad(string):
         self.checkLength(string, 3)
         return unpack('>L', b'\x00' + string)[0]
 
-    @staticmethod
     def writeTriad(value):
         return pack('>L', value)[1:]
 
-    @staticmethod
     def readLTriad(string):
         self.checkLength(string, 3)
         return unpack('<L', b'\x00' + string)[0]
 
-    @staticmethod
     def writeLTriad(value):
         return pack('<L', value)[0:-1]
     
-    @staticmethod
     def readBool(b):
         return unpack('?', b)[0]
 
-    @staticmethod
     def writeBool(b):
         return b'\x01' if b else b'\x00'
     
-    @staticmethod
     def readByte(c, signed=True):
         self.checkLength(c, 1)
         if signed:
@@ -58,105 +49,83 @@ class Binary:
         else:
             return pack(">B", c)
 
-    @staticmethod
     def writeByte(c):
         return chr(c)
     
-    @staticmethod
     def readShort(string):
         self.checkLength(string, 2)
         return unpack('>H', string)[0]
 
-    @staticmethod
     def writeShort(value):
         return pack('>H', value)
     
-    @staticmethod
     def readLShort(string):
         self.checkLength(string, 2)
         return unpack('<H', string)[0]
 
-    @staticmethod
     def writeLShort(value):
         return pack('<H', value)
     
-    @staticmethod
     def readInt(string):
         self.checkLength(string, 4)
         return unpack('>L', string)[0]
 
-    @staticmethod
     def writeInt(value):
         return pack('>L', value)
 
-    @staticmethod
     def readLInt(string):
         self.checkLength(string, 4)
         return unpack('<L', string)[0]
 
-    @staticmethod
     def writeLInt(value):
         return pack('<L', value)
 
-    @staticmethod
     def readFloat(string):
         self.checkLength(string, 4)
         return unpack('>f', string)[0]
 
-    @staticmethod
     def writeFloat(value):
         return pack('>f', value)
 
-    @staticmethod
     def readLFloat(string):
         self.checkLength(string, 4)
         return unpack('<f', string)[0]
 
-    @staticmethod
     def writeLFloat(value):
         return pack('<f', value)
 
-    @staticmethod
     def readDouble(string):
         self.checkLength(string, 8)
         return unpack('>d', string)[0]
 
-    @staticmethod
     def writeDouble(value):
         return pack('>d', value)
 
-    @staticmethod
     def readLDouble(string):
         self.checkLength(string, 8)
         return unpack('<d', string)[0]
 
-    @staticmethod
     def writeLDouble(value):
         return pack('<d', value)
 
-    @staticmethod
     def readLong(string):
         self.checkLength(string, 8)
         return unpack('>l', string)[0]
 
-    @staticmethod
     def writeLong(value):
         return pack('>l', value)
 
-    @staticmethod
     def readLLong(string):
         self.checkLength(string, 8)
         return unpack('<l', string)[0]
 
-    @staticmethod
     def writeLLong(value):
         return pack('<l', value)
     
-    @staticmethod
     def readUnsignedVarint(stream):
         value = 0;
         i = 0
-        while(true):
+        while(True):
             if(i > 63):
                 raise ValueError('Varint did not terminate after 10 bytes!')
             b = stream.encode()
@@ -167,7 +136,6 @@ class Binary:
     
         return value
     
-    @staticmethod
     def writeUnsignedVarint(value):
         buf = ""
         for i in range(0, 10):
@@ -180,7 +148,6 @@ class Binary:
             value = ((value >> 7) & (sys.maxint >> 6))  
         raise ValueError('Value too large to be encoded as a varint')
         
-    @staticmethod
     def readVarint(stream):
         intsize = sys.getsizeof(int()) == 8
         shift = intsize if 63 != None else 31
@@ -188,7 +155,6 @@ class Binary:
         temp = (((raw << shift) >> shift) ^ raw) >> 1
         return temp ^ (raw & (1 << shift))
     
-    @staticmethod
     def writeVarint(v):
         intsize = sys.getsizeof(int()) == 8
         return self.writeUnsignedVarInt((v << 1) ^ (v >> (intsize if 63 != None else 31)))
