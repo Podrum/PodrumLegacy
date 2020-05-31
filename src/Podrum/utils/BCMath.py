7cd02e3cd9fa6d4ac34fdbf4d8a10e39b405e4fd
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-05-31 09:35:25 +0300

    Update BCMath.py

--- src/Podrum/utils/BCMath.py
+++ src/Podrum/utils/BCMath.py
@@ -1,1 +1,39 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
+import decimal
+
+class BCMath:
+    @staticmethod
+    def bcmul(num1, num2):
+        result = decimal.Decimal(num1) * decimal.Decimal(num2)
+        return result
+      
+    @staticmethod
+    def bcdiv(num1, num2, scale=None):
+        if num3 != None:
+            decimal.getcontext().prec = scale
+        result = decimal.Decimal(num1) / decimal.Decimal(num2)
+        return result
+       
+    @staticmethod
+    def bcadd(num1, num2):
+        result = decimal.Decimal(num1) + decimal.Decimal(num2)
+        return result
+    
+    @staticmethod
+    def bcsub(num1, num2, scale=None):
+        if num3 != None:
+            decimal.getcontext().prec = scale
+        result = decimal.Decimal(num1) - decimal.Decimal(num2)
+        return result
 
