56524028387a8623e724fff4eb9699c58ac38f9d
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-06-01 17:18:50 -0500

    Delete start_travis.py

--- start_travis.py
+++
@@ -1,7 +0,0 @@
-from os import getcwd
-from threading import Thread
-from src.Podrum.Server import Server
-
-if __name__ == "__main__":
-    serverThread = Thread(target=Server, args=(getcwd(),))
-    serverThread.start()
