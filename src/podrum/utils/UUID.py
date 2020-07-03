d896a777f9c056731732a344af632e181b904832
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-07-03 10:26:53 +0300

    Update UUID.py

--- src/podrum/utils/UUID.py
+++ src/podrum/utils/UUID.py
@@ -1,1 +1,23 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
 
+class UUID:
+    parts = [0, 0, 0, 0]
+    version = None
+    
+    def __init__(self, part1 = 0, part2 = 0, part3 = 0, part4 = 0, version = None):
+        self.parts[0] = int(part1)
+        self.parts[1] = int(part2)
+        self.parts[2] = int(part3)
+        self.parts[3] = int(part4)
+        self.version = (self.parts[1] & 0xf000) >> 12 if version == None else int(version)
