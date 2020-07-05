37bebd90d5a72071e45d3ca03c0fbe5bced07841
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-05 16:01:04 -0700

    Update PlayStatusPacket.py

--- src/podrum/network/protocol/PlayStatusPacket.py
+++ src/podrum/network/protocol/PlayStatusPacket.py
@@ -16,3 +16,23 @@ from podrum.network.protocol.ProtocolInfo import ProtocolInfo
 
 class PlayStatusPacket(DataPacket):
     NID = ProtocolInfo.PLAY_STATUS_PACKET
+
+    LOGIN_SUCCESS = 0
+    LOGIN_FAILED_CLIENT = 1
+    LOGIN_FAILED_SERVER = 2
+    PLAYER_SPAWN = 3
+    LOGIN_FAILED_INVALID_TENANT = 4
+    LOGIN_FAILED_VANILLA_EDU = 5
+    LOGIN_FAILED_EDU_VANILLA = 6
+    LOGIN_FAILED_SERVER_FULL = 7
+
+    status = None
+
+    def decodePayload(self):
+        self.status = self.getInt()
+
+    def canBeSentBeforeLogin():
+        return True
+
+    def encodePayload(self):
+        self.putInt(self.status)
