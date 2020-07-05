d5101c40fb1d539eeeb80b00834185cdf46647b0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-05 15:56:18 -0700

    Create PlayStatusPacket.py

---
+++ src/podrum/network/protocol/PlayStatusPacket.py
@@ -0,0 +1,18 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
+
+from podrum.network.protocol.DataPacket import DataPacket
+from podrum.network.protocol.ProtocolInfo import ProtocolInfo
+
+class PlayStatusPacket(DataPacket):
+    NID = ProtocolInfo.PLAY_STATUS_PACKET
