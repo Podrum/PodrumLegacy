29f22fd0579f11eed1d86c113d030234059550bc
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-05 19:09:53 -0700

    Create ResourcePacksInfoPacket.py

---
+++ src/podrum/network/protocol/ResourcePacksInfoPacket.py
@@ -0,0 +1,71 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
+
+from podrum.network.protocol.DataPacket import DataPacket
+from podrum.network.protocol.ProtocolInfo import ProtocolInfo
+
+class ResourcePacksInfoPacket(DataPacket):
+    NID = ProtocolInfo.RESOURCE_PACKS_INFO_PACKET
+
+    mustAccept = False
+    hasScripts = False
+    behaviorPackEntries = []
+    resourcePackEntries = []
+
+    def decodePayload(self):
+        self.mustAccept = self.getBool()
+        self.hasScripts = self.getBool()
+        behaviorPackCount = self.getLShort()
+        while behaviorPackCount > 0:
+            behaviorPackCount -= 1
+            self.getString()
+            self.getString()
+            self.getLLong()
+            self.getString()
+            self.getString()
+            self.getString()
+            self.getBool()
+
+        resourcePackCount = self.getLShort()
+        while resourcePackCount > 0:
+            resourcePackCount -= 1
+            self.getString()
+            self.getString()
+            self.getLLong()
+            self.getString()
+            self.getString()
+            self.getString()
+            self.getBool()
+
+    def encodePayload(self):
+        self.putBool(self.mustAccept)
+        self.putBool(self.hasScripts)
+        self.putLShort(len(self.behaviorPackEntries))
+        for entry in self.behaviorPackEntries:
+            self.putString(entry.getPackId())
+            self.putString(entry.getPackVersion())
+            self.putLLong(entry.getPackSize())
+            self.putString("") # TODO: encryption key
+            self.putString("") # TODO: subpack name
+            self.putString("") # TODO: content identity
+            self.putBool(False) # TODO: has scripts (?)
+
+        self.putLShort(len(self.resourcePackEntries))
+        for entry in self.resourcePackEntries:
+            self.putString(entry.getPackId())
+            self.putString(entry.getPackVersion())
+            self.putLLong(entry.getPackSize())
+            self.putString("") # TODO: encryption key
+            self.putString("") # TODO: subpack name
+            self.putString("") # TODO: content identity
+            self.putBool(False) # TODO: seems useless for resource packs
