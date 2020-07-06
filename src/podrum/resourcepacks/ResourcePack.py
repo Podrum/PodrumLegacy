5143c59fda95d0394c58ee71b504b8a18abed640
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-06 16:27:57 -0700

    Create ResourcePack.py

---
+++ src/podrum/resourcepacks/ResourcePack.py
@@ -0,0 +1,25 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
+
+class ResourcePack:
+    def getPath(): pass
+
+    def getPackName(): pass
+
+    def getPackId(): pass
+
+    def getPackSize(): pass
+
+    def getPackVersion(): pass
+
+    def getSha256(): pass
