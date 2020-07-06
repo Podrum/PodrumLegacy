f29414a7428bf089d09623db3c4078037e51b7c1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-07-06 23:25:54 +0300

    Update BatchPacket.py

--- src/podrum/network/protocol/BatchPacket.py
+++ src/podrum/network/protocol/BatchPacket.py
@@ -1,1 +1,21 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
 
+from podrum.network.PacketPool import PacketPool
+from podrum.network.protocol.DataPacket import DataPacket
+from podrum.network.protocol.ProtocolInfo import ProtocolInfo
+
+class BatchPacket:
+    NID = ProtocolInfo.BATCH_PACKET
+    
+    
