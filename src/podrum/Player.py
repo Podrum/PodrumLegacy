33187b2a2969ac7a86fbbddf9160abeecf196672
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-07-07 12:08:14 +0300

    Update Player.py

--- src/podrum/Player.py
+++ src/podrum/Player.py
@@ -1,1 +1,42 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+"""
 
+from podrum.network.PacketPool import PacketPool
+
+class Player:
+    connection = None
+    server = None
+    logger = None
+    address = Nome
+    name = None
+    locale = None
+    randomId = None
+    uuid = None
+    xuid = None
+    skin = None
+    viewDistance = None
+    gamemode = 0
+    pitch = 0
+    yaw = 0
+    headYaw = 0
+    onGround = False
+    platformChatId = ''
+    deviceOS = None
+    deviceModel = None
+    deviceId = Nome
+    
+    def __init__(self, connection, address, logger, server):
+        self.connection = connection
+        self.address = address
+        self.logger = logger
+        self.server = server
