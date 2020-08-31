4f9bf83424d80c4781cbfe5211f10ff11a420e70
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-01 01:24:28 +0300

    Create UnknownPacket.py

---
+++ src/podrum/network/protocol/UnknownPacket.py
@@ -0,0 +1,24 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from podrum.network.protocol.DataPacket import DataPacket
+
+class UnknownPacket(DataPacket):
+    NID = -1
+
+    payload = None
+
+    def decodePayload(self):
+        pass
+
+    def encodePayload(self):
+        pass
