212197888e7d426a0ae7b7c8e15acfdd97deeec8
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-01 01:18:00 +0300

    Update NetworkInterface.py

--- src/podrum/network/NetworkInterface.py
+++ src/podrum/network/NetworkInterface.py
@@ -1,1 +1,26 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
 
+from copy import deepcopy
+
+from podrum.network.PacketPool import PacketPool
+from podrum.utils.Binary import Binary
+
+class NetworkInterface:
+    server = None
+    players = []
+    identifers = None
+    identifiersACK = []
+    interface = None
+    tickTask = None
+    upload = 0
+    download = 0
