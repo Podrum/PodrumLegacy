3f1861d6e3cc51112b603267fbcfe984cf74b82a
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-04 01:06:27 +0300

    Update QueryHandler.py

--- src/podrum/network/QueryHandler.py
+++ src/podrum/network/QueryHandler.py
@@ -1,1 +1,28 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
 
+from podrum import Server
+from podrum.utils.Binary import Binary
+from podrum.utils.Utils import Utils
+
+class QueryHandler:
+    server = None
+    lastToken = None
+    token = None
+    longData = None
+    timeout = None
+    
+    HANDSHAKE = 9
+    STATISTICS = 0
+    
+    def __init__(self):
+        self.server = Server.Server()
