f7e9349edaab76f4587e367f2f8ee3c3e7b63cb4
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-27 07:24:07 +0300

    Update NetBinaryStream.py

--- src/podrum/network/NetBinaryStream.py
+++ src/podrum/network/NetBinaryStream.py
@@ -10,7 +10,7 @@
 * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 """
 
-from podrum.utils.BinaryStream import BinaryStream
+from binutilspy.BinaryStream import BinaryStream
 from podrum.utils.UUID import UUID
 
 class NetBinaryStream(BinaryStream):
