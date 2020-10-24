8eb5e876bd55a5d71092ed00efb77558e8964761
StarlkYT <45287860+StarlkYT@users.noreply.github.com>
2020-10-24 22:05:47 +0300

    Update ProtocolInfo.py

--- src/podrum/network/mcje/protocol/ProtocolInfo.py
+++ src/podrum/network/mcje/protocol/ProtocolInfo.py
@@ -1,3 +1,10 @@
 class ProtocolInfo:
+    # Versions info
     CURRENT_PROTOCOL = 753
+    VERSION = "v1.16.3"
+    VERSION_NETWORK = "1.16.3"
+    
+    # Protocol info
+    HANDSHAKING = 0x00
+    PONG = 0x01
     
