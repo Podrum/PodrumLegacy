5ba3526deeb81e4475e5f4d5d9f51f46f5902589
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-14 01:50:15 +0200

    Create EncapsulatedPacket.py

---
+++ src/podrum/network/raknet/protocol/EncapsulatedPacket.py
@@ -0,0 +1,32 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+class EncapsulatedPacket:
+    buffer = None
+    messageIndex = None
+    sequenceIndex = None
+    orderIndex = None
+    orderChannel = None
+    split = None
+    splitCount = None
+    splitIndex = None
+    splitId = None
+    
+    @staticmethod
+    def fromBinary(stream):
+        packet = EncapsulatedPacket()
+        header = stream.getByte()
+        packet.reliability = (header & 224) >> 5
+        packet.split = (header & 0x10) > 0
+        length = stream.getShort() >> 3
+        if length == 0:
+            raise Exception("Got an empty encapsulated packet")       
