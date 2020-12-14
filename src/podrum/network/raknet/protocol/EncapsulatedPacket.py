aaa0a4fbe9a7203f848a473fa4f79784ce039f7c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-14 02:18:42 +0200

    Update EncapsulatedPacket.py

--- src/podrum/network/raknet/protocol/EncapsulatedPacket.py
+++ src/podrum/network/raknet/protocol/EncapsulatedPacket.py
@@ -10,6 +10,7 @@
 * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 """
 
+from binutilspy.BinaryStream import BinaryStream
 from podrum.network.raknet.Reliability import Reliability
 
 class EncapsulatedPacket:
@@ -46,3 +47,28 @@ class EncapsulatedPacket:
         packet.buffer = stream.buffer[stream.offset:]
         stream.offset += length
         return packet
+
+    def toBinary(self):
+        stream = BinaryStream()
+        header = self.reliability << 5
+        if self.split:
+            header |= 0x10
+        stream.putByte(header)
+        stream.putShort(len(self.buffer) << 3)
+        if Reliability.isReliable(packet.reliability):
+            stream.putLTriad(packet.messageIndex)
+        if Reliability.isSequenced(packet.reliability):
+            stream.putLTriad(packet.sequenceIndex)
+        if Reliability.isSequencedOrOrdered(packet.reliability):
+            stream.putLTriad(packet.orderIndex)
+            stream.putByte(packet.orderChannel)
+        if packet.split:
+            stream.putInt(packet.splitCount)
+            stream.putShort(packet.splitId)
+            stream.putInt(packet.splitIndex)
+        stream.put(self.buffer)
+        return stream
+    
+    def getTotalLength(self):
+        length = 3
+        length += 
