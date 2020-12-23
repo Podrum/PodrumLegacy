39a231d2196d01bea8a0393519b892da15ee10b0
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-24 00:14:13 +0200

    Update TaskManager.py

--- src/podrum/task/TaskManager.py
+++ src/podrum/task/TaskManager.py
@@ -10,9 +10,6 @@
 * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 """
 
-from Task import Task
-from RepeatingTask import RepeatingTask
-
 class TaskManager:
     tasks = {}
     
