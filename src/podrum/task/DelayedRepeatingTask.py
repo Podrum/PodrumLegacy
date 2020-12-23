d90f0841973811be8475f9256a0e82d9ef944a9e
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-24 00:49:34 +0200

    Create DelayedRepeatingTask.py

---
+++ src/podrum/task/DelayedRepeatingTask.py
@@ -0,0 +1,30 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from podrum.task.Task import Task
+from time import sleep
+
+class DelayedRepeatingTask(Task):
+    delay = None
+    isRunning = True
+
+    def __init__(self, taskObject, taskId, delay):
+        self.delay = delay
+        super().__init__(taskObject, taskId)
+        
+    def stop(self):
+        self.isRunning = False
+
+    def run(self):
+        while self.isRunning:
+            sleep(self.delay)
+            self.taskObject()
