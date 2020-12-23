5373d4ff1b713767a07b41712b0b08c912e6bda3
CodiiiDev <68484147+CodiiiDev@users.noreply.github.com>
2020-12-23 23:04:13 +0100

    Add files via upload

---
+++ src/podrum/task/RepeatingTask.py
@@ -0,0 +1,31 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from threading import Thread
+import time
+from Task import Task
+
+class RepeatingTask(Task):
+    interval = None
+    isRunning = True
+
+    def __init__(self, taskObject, taskId, interval = 0):
+        self.interval = interval
+        super().__init__(taskObject, taskId)
+        
+    def stop(self):
+        self.isRunning = False
+
+    def run(self):
+        while self.isRunning:
+            self.taskObject()
+            time.sleep(self.interval)
---
+++ src/podrum/task/TaskManager.py
@@ -0,0 +1,31 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from Task import Task
+from RepeatingTask import RepeatingTask
+
+class TaskManager:
+    tasks = {}
+    
+    @staticmethod
+    def addTask(taskId, taskThread):
+        TaskManager.tasks[taskId] = taskThread
+        
+    @staticmethod
+    def removeTask(taskId):
+        if taskId in self.tasks:
+            del TaskManager.tasks[taskId]
+            
+    @staticmethod
+    def getTask(taskId):
+        if taskId in self.tasks:
+            return TaskManager.tasks[taskId]
