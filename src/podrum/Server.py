847e40be9154afb3f5c11e8a2bae7740f033d937
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-27 13:04:26 +0200

    Update Server.py

--- src/podrum/Server.py
+++ src/podrum/Server.py
@@ -13,9 +13,6 @@
 from podrum.command.CommandReader import CommandReader
 from podrum.GeneralVariables import GeneralVariables
 from podrum.lang.Base import Base
-from podrum.network.PacketPool import PacketPool as Pool
-from podrum.network.NetworkInterface import NetworkInterface
-from podrum.Player import Player
 from podrum.plugin.Plugin import Plugin
 from podrum.utils.Logger import Logger
 from podrum.utils.Utils import Utils
@@ -25,13 +22,10 @@ from podrum.wizard.Wizard import Wizard
 class Server:
     tickrate = 1000 / 20
     isTicking = True
-    addr = "0.0.0.0"
-    port = 19132
     players = []
     startTime = None
     endTime = None
     timeDiff = None
-    mainInterface = None
     podrumLogo = """
             ____           _                      
            |  _ \ ___   __| |_ __ _   _ _ __ ___  
@@ -40,10 +34,8 @@ class Server:
            |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
     """
 
-    def __init__(self, withWizard, isTravisBuild = False):
+    def __init__(self, withWizard):
         super().__init__()
-        if isTravisBuild:
-            exit(0)
         self.startTime = time()
         if Utils.getOS() == "windows":
             Utils.enableWindowsFormating()
@@ -58,13 +50,11 @@ class Server:
                 pass
         print(str(self.podrumLogo))
         Logger.info(str(Base.getTranslation("startingServer")).replace("{ip}", str(Utils.getPrivateIpAddress())).replace("{port}", str(self.port)))
-        Logger.info(str(Base.getTranslation("extIpMsg")).replace("{ipPublic}", str(Utils.getPublicIpAddress())))
         Logger.info(str(Base.getTranslation("license")))
         GeneralVariables.server = self
         GeneralVariables.plugin = Plugin("./plugins", self)
         GeneralVariables.plugin.loadAll()
         self.endTime = time()
-        self.mainInterface = NetworkInterface()
         self.timeDiff = "%.3f" % (self.endTime - self.startTime)
         Logger.info(f'Done in {str(self.timeDiff)}s. Type "help" to view all available commands.')
         CommandReader(self)
@@ -73,12 +63,6 @@ class Server:
                 
     def getLogger(self):
         return Logger()
-    
-    def getAddress(self):
-        return self.addr
-    
-    def getPort(self):
-        return self.port
 
     def sendMessage(self, message):
         self.getLogger().log("info", message)
