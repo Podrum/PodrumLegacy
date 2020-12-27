fb5ca7200814dcb95026f9708a7b514273696778
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-27 23:22:17 +0200

    Update LoginPacket.py

--- src/podrum/network/protocol/LoginPacket.py
+++ src/podrum/network/protocol/LoginPacket.py
@@ -39,33 +39,7 @@ class LoginPacket(DataPacket):
         return self.protocol is not None and self.protocol != ProtocolInfo.MCBE_PROTOCOL_VERSION
         
     def decodePayload(self):
-        self.protocol = self.getInt()
-        try:
-            buffer = DataPacket.NetBinaryStream(self.getString())
-            self.chainData = json.loads(buffer.get(buffer.getLInt()))
-            hasExtraData = False
-            for chain in self.chainData["chain"]:
-                webtoken = Utils.decodeJWT(chain)
-                if webtoken["extraData"] in locals() or webtoken["extraData"] in globals():
-                    if hasExtraData:
-                        raise Exception("Found 'extraData' multiple times in key chain")
-                    hasExtraData = True
-                    if webtoken["extraData"]["displayName"] in locals() or webtoken["extraData"]["displayName"] in globals():
-                        self.username = webtoken["extraData"]["displayName"]
-                    if webtoken["extraData"]["identity"] in locals() or webtoken["extraData"]["identity"] in globals():
-                        self.clientUUID = webtoken["extraData"]["identity"]
-                    if webtoken["extraData"]["XUID"] in locals() or webtoken["extraData"]["XUID"] in globals():
-                        self.xuid = webtoken["extraData"]["XUID"]
-                if webtoken["identityPublicKey"] in locals() or webtoken["identityPublicKey"] in globals():
-                    self.identityPublicKey = webtoken["identityPublicKey"]
-            self.clientDataJwt = buffer.get(buffer.getLInt())
-            self.clientData = Utils.decodeJWT(self.clientDataJwt)
-            self.clientId = self.clientData["ClientRandomId"] if self.clientData["ClientRandomId"] is not None else None
-            self.serverAddress = self.clientData["ServerAddress"] if self.clientData["ServerAddress"] is not None else None
-            self.locale = self.clientData["LanguageCode"] if self.clientData["LanguageCode"] is not None else None
-        except Exception as e:
-            if self.protocol == ProtocolInfo.MCBE_PROTOCOL_VERSION:
-                raise Exception(f"Error: {str(e)}")
+        # T O D O #
                 
     def encodePayload(self): 
         pass
