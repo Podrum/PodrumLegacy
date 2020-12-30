6b6ffdc3116d162f91f66aed56d83f9395da786a
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-30 20:11:49 +0200

    Update PluginBase.py

--- src/podrum/plugin/PluginBase.py
+++ src/podrum/plugin/PluginBase.py
@@ -4,21 +4,6 @@ class PluginBase:
     author = ""
     version = ""
     server = None
-
-    def getName(self):
-        return self.name
-
-    def getDescription(self):
-        return self.description
-
-    def getAuthor(self):
-        return self.author
-
-    def getVersion(self):
-        return self.version
-    
-    def getServer(self):
-        return self.server
     
     def onLoad(self):
         pass
