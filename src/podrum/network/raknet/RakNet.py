8a11808f0fcccca769af5483f90463ad8f5cf9f1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-07 02:49:25 +0200

    Create RakNet.py

---
+++ src/podrum/network/raknet/RakNet.py
@@ -0,0 +1,26 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Mozilla Public License, Version 2.
+* Permissions of this weak copyleft license are conditioned on making
+* available source code of licensed files and modifications of those files 
+* under the same license (or in certain cases, one of the GNU licenses).
+* Copyright and license notices must be preserved. Contributors
+* provide an express grant of patent rights. However, a larger work
+* using the licensed work may be distributed under different terms and without 
+* source code for files added in the larger work.
+"""
+
+class RakNet:
+    protocol = 10
+    flagValid = 0x80
+    flagAck = 0x40
+    flagNack = 0x20
+    flagSplit = 0x10
+    flagNeedAck = 0x08
+    flagPriority = 0x07
+    systemAddressCount = 20
