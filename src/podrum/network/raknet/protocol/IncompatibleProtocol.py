8513beb2d0596294e2cc52c8abae93bea12d78df
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-07 16:31:21 +0200

    Create IncompatibleProtocol.py

---
+++ src/podrum/network/raknet/protocol/IncompatibleProtocol.py
@@ -0,0 +1,34 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Mozilla Public License, Version 2.
+* Permissions of this weak copyleft license are conditioned on making
+* available source code of licensed files and modifications of those files 
+* under the same license (or in certain cases, one of the GNU licenses).
+* Copyright and license notices must be preserved. Contributors
+* provide an express grant of patent rights. However, a larger work
+* using the licensed work may be distributed under different terms and without 
+* source code for files added in the larger work.
+"""
+
+from podrum.network.raknet.protocol.OfflinePacket import OfflinePacket
+from podrum.network.raknet.protocol.PacketIdentifiers import PacketIdentifiers
+
+class IncompatibleProtocol(OfflinePacket):
+    id = PacketIdentifiers.incompatibleProtocol
+    protocol = None
+    serverGuid = None
+    
+    def decodePayload(self):
+        self.protocol = self.getByte()
+        self.getMagic()
+        self.serverGuid = self.getLong()
+        
+    def encodePayload(self):
+        self.putByte(self.protocol)
+        self.putMagic()
+        self.putLong(self.serverGuid)
