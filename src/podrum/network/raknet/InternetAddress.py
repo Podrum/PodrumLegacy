bf5caba3d6cf1f62adcfa1474fec04391ca6e6e3
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-07 02:58:11 +0200

    Create InternetAddress.py

---
+++ src/podrum/network/raknet/InternetAddress.py
@@ -0,0 +1,26 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Mozilla Public License, Version 2.
+* Permissions of this weak copyleft license are conditioned on making
+* available source code of licensed files and modifications of those files 
+* under the same license (or in certain cases, one of the GNU licenses).
+* Copyright and license notices must be preserved. Contributors
+* provide an express grant of patent rights. However, a larger work
+* using the licensed work may be distributed under different terms and without 
+* source code for files added in the larger work.
+"""
+
+class InternetAddress:
+    ip = None
+    port = None
+    version = None
+    
+    def __init__(self, ip, port, version = 4):
+        self.ip = ip
+        self.port = port
+        self.version = version
