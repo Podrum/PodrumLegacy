81be9bfbb356ad3bd53dcc7f52e9f4ae9f10134e
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-08 14:14:07 +0200

    Create OnlinePing.py

---
+++ src/podrum/network/raknet/protocol/OnlinePing.py
@@ -0,0 +1,29 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Mozilla Public License, Version 2.
+* Permissions of this weak copyleft license are conditioned on making
+* available source code of licensed files and modifications of those files 
+* under the same license (or in certain cases, one of the GNU licenses).
+* Copyright and license notices must be preserved. Contributors
+* provide an express grant of patent rights. However, a larger work
+* using the licensed work may be distributed under different terms and without 
+* source code for files added in the larger work.
+"""
+
+from podrum.network.raknet.protocol.Packet import Packet
+from podrum.network.raknet.protocol.PacketIdentifiers import PacketIdentifiers
+
+class OnlinePing(Packet):
+    id = PacketIdentifiers.onlinePing
+    timestamp = None
+    
+    def decodePayload(self):
+        self.timestamp = self.getLong()
+        
+    def encodePayload(self):
+        self.putLong(self.timestamp)
