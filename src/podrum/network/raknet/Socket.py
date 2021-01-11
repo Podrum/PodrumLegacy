bab11ebe091e9ea5f35f1383397d21815635cd5e
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-11 23:05:04 +0200

    Update Socket.py

--- src/podrum/network/raknet/Socket.py
+++ src/podrum/network/raknet/Socket.py
@@ -15,7 +15,41 @@
 * source code for files added in the larger work.
 """
 
+from podrum.network.raknet.InternetAddress import InternetAddress
+from podrum.utils.BinaryStream import BinaryStream
 import socket
 
 class Socket:
-    pass
+    address
+    socket
+    
+    def __init__(self, address):
+        self.address = address
+        self.socket = socket.socket(socket.AF_INET if address.version == 4 else socket.AF_INET6, socket.SOCK_DGRAM, socket.SOL_UDP)
+        if address.version == 6:
+            self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+        try:
+            self.socket.bind((self.address.address, self.address.port))
+        except socket.error as e:
+            print(f"Unable to binto to {str(address.port)}")
+            print(str(e))
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024 * 1024 * 8)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024 * 1024 * 8)
+        
+    def receive(self):
+        try:
+            buffer, source = self.socket.recvfrom(65535, 0)
+            return (BinaryStream(buffer), InternetAddress(source[0], source[1]))
+        except socket.error:
+            pass
+          
+    def send(self, stream, address):
+        try:
+            buffer = stream.buffer
+            source = (address.ip, address.port)
+            return self.socket.sendto(buffer, source)
+        except socket.error:
+            pass
+    
+    def close(self):
+        self.socket.close()
