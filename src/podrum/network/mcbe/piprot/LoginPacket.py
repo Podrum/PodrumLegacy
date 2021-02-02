bb6795b2f5a9ea2485efb8016eb13eea5bdd2daa
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-02 16:41:44 +0200

    Create LoginPacket.py

---
+++ src/podrum/network/mcbe/piprot/LoginPacket.py
@@ -0,0 +1,31 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Mozilla Public License, Version 2.
+* Permissions of this weak copyleft license are conditioned on making
+* available source code of licensed files and modifications of those files 
+* under the same license (or in certain cases, one of the GNU licenses).
+* Copyright and license notices must be preserved. Contributors
+* provide an express grant of patent rights. However, a larger work
+* using the licensed work may be distributed under different terms and without 
+* source code for files added in the larger work.
+"""
+
+from podrum.network.mcbe.piprot.Info import Info
+from podrum.network.mcbe.piprot.Packet import Packet
+
+class LoginPacket(Packet):
+    id = Info.loginPacket
+    username = None
+    protocol1 = None
+    protocol2 = None
+    
+    def decodePayload(self):
+        pass
+        
+    def encodePayload(self):
+        pass
