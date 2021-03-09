28aa79b1fae41b65f4962e4cd846b57425198d9c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-09 15:26:40 +0200

    Update acknowledgement.py

--- podrum/packet/raknet/acknowledgement.py
+++ podrum/packet/raknet/acknowledgement.py
@@ -50,16 +50,34 @@ class acknowledgement(protocol_buffer):
     def write_data(self) -> None:
         self.write_uchar(self.packet_id)
         self.sequence_numbers.sort()
-        temp_buffer = protocol_buffer()
-        count = 0
-        for i in range(0, len(self.sequence_numbers), 2):
-            if len(self.sequence_numbers[i:]) > 1:
-                temp_buffer.write_bool(False)
-                temp_buffer.write_utriad(self.sequence_numbers[i], "little")
-                temp_buffer.write_utriad(self.sequence_numbers[i + 1], "little")
-            else:
+        temp_buffer: object = protocol_buffer()
+        count: int = 0
+        if len(self.sequence_numbers) > 0:
+            start_index = self.sequence_numbers[0]
+            end_index = self.sequence_numbers[0]
+            for pointer in range(1, len(self.sequence_numbers)):
+                current_index = self.sequence_numbers[pointer]
+                diff = current_index - end_index
+                if diff == 1:
+                    end_index = current_index
+                elif diff > 1:
+                    if start_index == end_index:
+                        temp_buffer.write_bool(True)
+                        temp_buffer.write_utriad(start_index, "little")
+                        start_index = end_index = current_index
+                    else:
+                        temp_buffer.write_bool(False)
+                        temp_buffer.write_utriad(start_index, "little")
+                        temp_buffer.write_utriad(end_index, "little")
+                        start_index = end_index = current_index
+                    count += 1
+            if start_index == end_index:
                 temp_buffer.write_bool(True)
-                temp_buffer.write_utriad(self.sequence_numbers[i], "little")
+                temp_buffer.write_utriad(start_index, "little")
+            else:
+                temp_buffer.write_bool(False)
+                temp_buffer.write_utriad(start_index, "little")
+                temp_buffer.write_utriad(end_index, "little")
             count += 1
-        self.write_ushort(count, "big")
-        self.write(temp_buffer.data)
+            self.write_ushort(count, "big")
+            self.write(temp_buffer.data)
