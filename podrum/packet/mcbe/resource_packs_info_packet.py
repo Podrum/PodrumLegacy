e9921e55508f1151f610167e09d04acd019175b8
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-15 01:20:30 +0200

    Create resource_packs_info_packet.py

---
+++ podrum/packet/mcbe/resource_packs_info_packet.py
@@ -0,0 +1,45 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+class resource_packs_info_packet:
+    def read_data(self):
+        self.packet_id: int = self.read_var_int()
+        self.forced_to_accept: bool = self.read_bool()
+        self.scripting_enabled: bool = self.read_bool()
+        resource_packs_count: int = self.read_unsigned_short_le()
+        for i in range(0, resource_packs_count):
+            pass # I dont need this yet
+          
+    def write_data(self):
+        self.write_var_int(self.packet_id)
+        self.write_bool(self.forced_to_accept)
+        self.write_bool(self.scripting_enabled)
+        self.write(b"\x00\x00\x00\x00") # I dont need this yet
