2770d1755dfb49bb0cc87b02d6bff2db9fb06ae5
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-17 23:00:37 +0200

    Update nbt_be_binary_stream.py

--- podrum/utils/nbt_be_binary_stream.py
+++ podrum/utils/nbt_be_binary_stream.py
@@ -108,5 +108,5 @@ class nbt_be_binary_stream(binary_stream):
         return self.read(self.read_unsigned_short_be()).decode()
       
     def write_string_tag(self, value: str) -> None:
-        self.writeunsigned_short_be(len(value))
+        self.write_unsigned_short_be(len(value))
         self.write(value.encode())
