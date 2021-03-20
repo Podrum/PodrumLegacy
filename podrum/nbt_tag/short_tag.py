eb5abbd3eb74f3a4c559bf8b4498237d3ef32bb0
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-20 17:14:50 +0200

    Update short_tag.py

--- podrum/nbt_tag/short_tag.py
+++ podrum/nbt_tag/short_tag.py
@@ -29,8 +29,11 @@
 #                                                                              #
 ################################################################################
 
+from constant.nbt_tag_ids import nbt_tag_ids
+
 class short_tag:
     def __init__(self, name: str = "", value: int = 0) -> None:
+        self.id: int = nbt_tag_ids.short_tag
         self.name: str = name
         self.value: int = value
         
