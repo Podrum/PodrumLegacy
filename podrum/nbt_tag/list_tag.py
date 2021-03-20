66d65367073d513488e57b56a9962804e880bee9
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-20 14:51:55 +0200

    Create list_tag.py

---
+++ podrum/nbt_tag/list_tag.py
@@ -0,0 +1,52 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from utils.nbt import nbt
+
+class list_tag:
+    def __init__(self, name = "", value = [], list_type: int):
+        self.name: str = name
+        self.value: list = value
+        self.list_type: int = list_type
+        
+    def read(self, stream: object) -> None:
+        self.list_type = stream.read_byte_tag()
+        list_size = stream.read_int_tag()
+        list_tag = [nbt.new_tag(list_type)] * list_size
+        for item in list_tag:
+            item.read(stream)
+        return list_tag
+        
+    def write(self, stream: object) -> None:
+        stream.write_byte_tag(self.list_type)
+        stream.write_int_tag(self.value)
+        for item in self.value:
+            item.write(stream)
