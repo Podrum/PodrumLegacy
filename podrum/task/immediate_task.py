c3724bde40648f43210a47cae94581400c0c1021
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-04-17 14:05:14 +0300

    Create immediate_task.py

---
+++ podrum/task/immediate_task.py
@@ -0,0 +1,49 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from threading import Thread
+from time import sleep
+
+class immediate_task(Thread):
+    def __init__(self, task_object: object, args: list = [], interval: int = 0, interval_before: bool = False) -> None:
+        super().__init__()
+        self.setDaemon(True)
+        self.__task_object: object = task_object
+        self.__args: list = args
+        self.__interval: int = interval
+        self.__interval_before: bool = interval_before
+
+    def run(self) -> None:
+        if self.__interval_before:
+            sleep(self.__interval)
+        self.__task_object(*self.__args)
+        if not self.__interval_before:
+            sleep(self.__interval)
