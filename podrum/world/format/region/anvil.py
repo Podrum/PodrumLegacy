82a61c181aa94a1c9c00547236f9099b147f41b2
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-18 16:02:57 +0300

    Update anvil.py

--- podrum/world/format/region/anvil.py
+++ podrum/world/format/region/anvil.py
@@ -30,10 +30,52 @@
 ################################################################################
 
 import math
+from nbt_utils.tag.byte_tag import byte_tag
+from nbt_utils.tag.compound_tag import compound_tag
+from nbt_utils.tag.int_tag import int_tag
+from nbt_utils.tag.long_tag import long_tag
+from nbt_utils.tag.string_tag import string_tag
+from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
+import os
+import random
+import sys
+import time
 
 class anvil:
     def __init__(self, world_dir: str) -> None:
-        pass
+        real_path: str = os.path.abspath(world_dir)
+        if not os.path.isdir(real_path):
+            os.mkdir(real_path)
+        if not isfile(os.path.join(real_path, "level.dat")):
+            self.create_level_options_file()
         
     def chunk_region_location(self, x: int, z: int) -> tuple:
         return math.ceil(x / 32), math.ceil(z / 32)
+    
+    def create_level_options_file(self, path: str) -> None:
+        stream: object = nbt_be_binary_stream(chunk)
+        tag: object = compound_tag("", [
+            compound_tag("Data", [
+                byte_tag("hardcore", 0),
+                byte_tag("MapFeatures", 0),
+                byte_tag("raining", 0),
+                byte_tag("thundering", 0),
+                int_tag("GameType", 0),
+                int_tag("generatorVersion", 0),
+                int_tag("rainTime", 0),
+                int_tag("SpawnX", 0),
+                int_tag("SpawnY", 0),
+                int_tag("SpawnZ", 0),
+                int_tag("thunderTime", 0),
+                int_tag("version", 19133),
+                long_tag("LastPlayed", int(time.time())),
+                long_tag("RandomSeed", random.randint(0, sys.maxsize)),
+                long_tag("SizeOnDisk", 0),
+                long_tag("Time", 0),
+                string_tag("generatorName", "flat"),
+                string_tag("LevelName", "world")
+            ])
+        ])
+        tag.write(stream)
+        file: object = open(path, "wb")
+        file.write(stream.data)
