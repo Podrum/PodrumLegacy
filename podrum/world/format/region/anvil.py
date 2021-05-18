b6d6810f32015a49c78fe98f47262b3a82b6cf36
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-18 17:19:29 +0300

    Update anvil.py

--- podrum/world/format/region/anvil.py
+++ podrum/world/format/region/anvil.py
@@ -37,6 +37,9 @@ from nbt_utils.tag.long_tag import long_tag
 from nbt_utils.tag.string_tag import string_tag
 from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
 import os
+from podrum.world.chunk.chunk import chunk
+from podrum.world.chunk.empty_sub_chunk import empty_sub_chunk
+from podrum.world.chunk.sub_chunk import sub_chunk
 from podrum.world.format.region.region import region
 import random
 import sys
