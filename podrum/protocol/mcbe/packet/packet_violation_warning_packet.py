23aad8cbf371754ecf19076798ea8877971a3ea9
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-29 22:19:54 +0300

    Update and rename podrum/packet/mcbe/packet_violation_warning_packet.py to podrum/protocol/mcbe/packet/packet_violation_warning_packet.py

--- podrum/protocol/mcbe/packet/packet_violation_warning_packet.py
+++ podrum/protocol/mcbe/packet/packet_violation_warning_packet.py
@@ -29,13 +29,13 @@
 #                                                                              #
 ################################################################################
 
-from constant.mcbe_packet_ids import mcbe_packet_ids
-from packet.mcbe.packet import packet
+from protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
+from protocol.mcbe.packet.mcbe_packet import mcbe_packet
 
-class packet_violation_warning_packet(packet):
+class packet_violation_warning_packet(mcbe_packet):
     def __init__(self, data: bytes = b"", pos: int = 0) -> None:
         super().__init__(data, pos)
-        self.packet_id: int = mcbe_packet_ids.packet_violation_warning_packet
+        self.packet_id: int = mcbe_protocol_info.packet_violation_warning_packet
 
     def decode_payload(self) -> None:
         self.type: int = self.read_signed_var_int()
