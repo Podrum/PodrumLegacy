0d21319dde4807d16da898f9647ef0e04b5456af
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-29 23:03:13 +0300

    Update biome_definition_list_packet.py

--- podrum/protocol/mcbe/packet/biome_definition_list_packet.py
+++ podrum/protocol/mcbe/packet/biome_definition_list_packet.py
@@ -30,7 +30,7 @@
 ################################################################################
 
 import base64
-from protocol.mcbe.packet.mcbe_protocol_info import mcbe_protocol_info
+from protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
 from protocol.mcbe.packet.mcbe_packet import mcbe_packet
 
 class biome_definition_list_packet(mcbe_packet):
