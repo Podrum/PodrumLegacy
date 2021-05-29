ce4167599a631d1ec179658aa85e3b8715595f5c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-29 22:29:30 +0300

    Update and rename podrum/packet/mcbe/game_packet.py to podrum/protocol/mcbe/packet/game_packet.py

--- podrum/protocol/mcbe/packet/game_packet.py
+++ podrum/protocol/mcbe/packet/game_packet.py
@@ -30,10 +30,10 @@
 ################################################################################
 
 from binary_utils.binary_stream import binary_stream
-from rak_net.protocol.packet import packet
+from protocol.mcbe.packet.mcbe_packet import mcbe_packet
 import zlib
 
-class game_packet(packet):
+class game_packet(mcbe_packet):
     def __init__(self, data: bytes = b"", pos: int = 0) -> None:
         super().__init__(data, pos)
         self.packet_id: int = 0xfe
