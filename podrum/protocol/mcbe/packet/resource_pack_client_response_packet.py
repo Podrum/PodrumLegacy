ae94dfc334c0838238400ada03ec73be27ef0c3f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-29 22:23:54 +0300

    Update and rename podrum/packet/mcbe/resource_pack_client_response_packet.py to podrum/protocol/mcbe/packet/resource_pack_client_response_packet.py

--- podrum/protocol/mcbe/packet/resource_pack_client_response_packet.py
+++ podrum/protocol/mcbe/packet/resource_pack_client_response_packet.py
@@ -29,13 +29,13 @@
 #                                                                              #
 ################################################################################
 
-from constant.mcbe_packet_ids import mcbe_packet_ids
-from packet.mcbe.packet import packet
+from protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
+from protocol.mcbe.packet.mcbe_packet import mcbe_packet
 
-class resource_pack_client_response_packet(packet):
+class resource_pack_client_response_packet(mcbe_packet):
     def __init__(self, data: bytes = b"", pos: int = 0) -> None:
         super().__init__(data, pos)
-        self.packet_id: int = mcbe_packet_ids.resource_pack_client_response_packet
+        self.packet_id: int = mcbe_protocol_info.resource_pack_client_response_packet
 
     def decode_payload(self):
         self.status: int = self.read_unsigned_byte()
