10b5da93e2fb918e8a850b327fd6b3aca5d985c3
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-02 19:18:38 +0300

    Update sub_chunk.py

--- podrum/world/chunk/sub_chunk.py
+++ podrum/world/chunk/sub_chunk.py
@@ -1,45 +1,27 @@
+from world.chunk.block_storage import block_storage
+
 class sub_chunk:
-    def __init__(self, ids: list = [], data: list = [], sky_light: list = [], block_light: list = []) -> None:
-        if len(ids) == 4096:
-            self.ids: list = ids
-        else:
-            self.ids: list = [0] * 4096
-        if len(data) == 2048:
-            self.data: list = data
-        else:
-            self.data: list = [0] * 2048
-        if len(sky_light) == 2048:
-            self.sky_light: list = sky_light
-        else:
-            self.sky_light: list = [255] * 2048
-        if len(block_light) == 2048:
-            self.block_light: list = block_light
-        else:
-            self.block_light: list = [0] * 2048
+    def __init__(self, block_storages: dict) -> None:
+        self.block_storages: object = block_storages
 
-    @staticmethod
-    def get_id_index(x: int, y: int, z: int) -> int:
-        return (x << 8) | (z << 4) | y
+    def get_block_storage(self, layer: int) -> object:
+        temp: int = (len(self.block_storages) - 1) - layer
+        needs: int = 0 if temp > 0 else abs(temp)
+        for i in range(0, needs):
+            self.block_storages.append(block_storage())
+        return self.block_storages[layer]
+    
+    def is_empty(self) -> bool:
+        return bool(len(self.block_storages))
+    
+    def get_block(self, x: int, y: int, z: int, layer: int) -> tuple:
+        return self.get_block_storage(layer).get_block(x, y & 0x0f, z)
+    
+    def get_block(self, x: int, y: int, z: int, block_id: int, meta: int, layer: int) -> None:
+        self.get_block_storage(layer).set_block(x, y & 0x0f, z, block_id, meta)
 
-    @staticmethod
-    def get_data_index(x: int, y: int, z: int) -> int:
-        return (x << 7) + (z << 3) + (y >> 1)
-
-    @staticmethod
-    def get_light_index(x: int, y: int, z: int) -> int:
-        return sub_chunk.get_data_index(x, y, z)
-
-    def set_block_id(self, x: int, y: int, z: int, block_id: int) -> None:
-        self.ids[sub_chunk.get_id_index(x, y, z)]: int = block_id
-
-    def get_highest_block_at(self, x: int, z: int) -> int:
-        low: int = (x << 8) | (z << 4)
-        i: int = low | 0x0f
-        while i >= low:
-            if self.ids[i] != 0x00:
-                return i & 0x0f
-            i -= 1
-        return -1
-
-    def encode(self) -> bytes:
-        return bytes([0] + self.ids + self.data)
+    def network_serialize(self, stream: object) -> None:
+        stream.write_unsigned_byte(8)
+        stream.write_unsigned_byte(len(self.block_storages))
+        for block_storage in self.block_storages:
+            block_storage.network_serialize(stream)
