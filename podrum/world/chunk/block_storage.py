8ddf826698fac3ff31f04e4ffe5e218dbde9f946
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-02 21:35:41 +0300

    Update block_storage.py

--- podrum/world/chunk/block_storage.py
+++ podrum/world/chunk/block_storage.py
@@ -60,7 +60,7 @@ class block_storage:
             self.palette.append(runtime_id)
         self.blocks[block_storage.get_index(x, y, x)]: int = self.palette.index(runtime_id)
 
-    def network_serialize(stream: object):
+    def network_serialize(self, stream: object):
         bits_per_block: int = math.ceil(math.log2(len(self.palette)))
         if bits_per_block == 0:
             bits_per_block: int = 1
