3ceac35c292e9c66c843d3f7e08292998c6a2b51
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-03 01:06:08 +0300

    Update mcbe_packet.py

--- podrum/protocol/mcbe/packet/mcbe_packet.py
+++ podrum/protocol/mcbe/packet/mcbe_packet.py
@@ -29,11 +29,9 @@
 #                                                                              #
 ################################################################################
 
-from binary_utils.binary_stream import binary_stream
-from geometry.vector_2 import vector_2
-from geometry.vector_3 import vector_3
+from protocol.mcbe.mcbe_binary_stream import mcbe_binary_stream
 
-class mcbe_packet(binary_stream):
+class mcbe_packet(mcbe_binary_stream):
     def decode_header(self) -> None:
         self.read_var_int()
       
@@ -49,97 +47,3 @@ class mcbe_packet(binary_stream):
         self.encode_header()
         if hasattr(self, "encode_payload"):
             self.encode_payload()
-
-    def read_string(self) -> str:
-        return self.read(self.read_var_int()).decode()
-    
-    def write_string(self, value: str) -> None:
-        self.write_var_int(len(value))
-        self.write(value.encode())
-        
-    def read_byte_array(self) -> bytes:
-        return self.read(self.read_var_int())
-    
-    def write_byte_array(self, value: bytes) -> None:
-        self.write_var_int(len(value))
-        self.write(value)
-        
-    def read_vector_2(self) -> object:
-        value: object = vector_2()
-        value.x: float = self.read_float_le()
-        value.z: float = self.read_float_le()
-        return value
-        
-    def write_vector_2(self, value: object) -> None:
-        self.write_float_le(value.x)
-        self.write_float_le(value.z)
-        
-    def read_vector_3(self) -> object:
-        value: object = vector_3()
-        value.x: float = self.read_float_le()
-        value.y: float = self.read_float_le()
-        value.z: float = self.read_float_le()
-        return value
-        
-    def write_vector_3(self, value: object) -> None:
-        self.write_float_le(value.x)
-        self.write_float_le(value.y)
-        self.write_float_le(value.z)
-        
-    def read_block_coordinates(self) -> object:
-        value: object = vector_3()
-        value.x: float = float(self.read_signed_var_int())
-        value.y: float = float(self.read_var_int())
-        value.z: float = float(self.read_signed_var_int())
-        return value
-        
-    def write_block_coordinates(self, value: object) -> None:
-        self.write_signed_var_int(int(value.x))
-        self.write_var_int(int(value.y))
-        self.write_signed_var_int(int(value.z))
-        
-    def read_player_location(self) -> dict:
-        value: dict = {}
-        value["x"]: int = self.read_float_le()
-        value["y"]: int = self.read_float_le()
-        value["z"]: int = self.read_float_le()
-        value["pitch"]: float = self.read_unsigned_byte() / 0.71
-        value["head_yaw"]: float = self.read_unsigned_byte() / 0.71
-        value["yaw"]: float = self.read_unsigned_byte() / 0.71
-        return value
-        
-    def write_player_location(self, value: dict) -> None:
-        self.write_float_le(value["x"])
-        self.write_float_le(value["y"])
-        self.write_float_le(value["z"])
-        self.write_unsigned_byte(int(value["pitch"] * 0.71))
-        self.write_unsigned_byte(int(value["head_yaw"] * 0.71))
-        self.write_unsigned_byte(int(value["yaw"] * 0.71))
-
-    def read_game_rules(self) -> dict:
-        rules_count: int = self.read_var_int()
-        rules: dict = {}
-        for i in range(0, rules_count):
-            rule_name: str = self.read_string()
-            rule_type: int = self.read_var_int()
-            if rule_type == 1:
-                rules[rule_name]: bool = self.read_bool()
-            elif rule_type == 2:
-                rules[rule_name]: int = self.read_var_int()
-            elif rule_type == 3:
-                rules[rule_name]: float = self.read_float_le()
-        return rules
-
-    def write_game_rules(self, rules: dict) -> None:
-        self.write_var_int(len(rules))
-        for rule_name, rule_value in rules.items():
-            self.write_string(rule_name)
-            if isinstance(rule_value, bool):
-                self.write_var_int(1)
-                self.write_bool(rule_value)
-            elif isinstance(rule_value, int):
-                self.write_var_int(2)
-                self.write_var_int(rule_value)
-            elif isinstance(rule_value, float):
-                self.write_var_int(3)
-                self.write_float_le(rule_value)
