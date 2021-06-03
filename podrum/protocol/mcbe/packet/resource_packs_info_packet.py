465c599e962925dea587dab2e2088dfc4760a770
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-03 15:23:52 +0300

    Update resource_packs_info_packet.py

--- podrum/protocol/mcbe/packet/resource_packs_info_packet.py
+++ podrum/protocol/mcbe/packet/resource_packs_info_packet.py
@@ -40,11 +40,11 @@ class resource_packs_info_packet(mcbe_packet):
     def decode_payload(self):
         self.forced_to_accept: bool = self.read_bool()
         self.scripting_enabled: bool = self.read_bool()
-        self.behavior_pack_info: list = self.read_behavior_pack_infos()
-        self.texture_packs_info: list = self.read_texture_pack_infos()
+        self.behavior_pack_infos: list = self.read_behavior_pack_infos()
+        self.texture_pack_infos: list = self.read_texture_pack_infos()
           
     def encode_payload(self):
         self.write_bool(self.forced_to_accept)
         self.write_bool(self.scripting_enabled)
-        self.write_behavior_pack_infos(self.behavior_pack_info)
-        self.write_texture_pack_infos(self.texture_packs_info)
+        self.write_behavior_pack_infos(self.behavior_pack_infos)
+        self.write_texture_pack_infos(self.texture_pack_infos)
