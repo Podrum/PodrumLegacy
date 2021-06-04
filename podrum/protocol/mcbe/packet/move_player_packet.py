0ac6f3f2a85986cc9df1f58f90c98500de6308d1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-04 14:25:27 +0300

    Update move_player_packet.py

--- podrum/protocol/mcbe/packet/move_player_packet.py
+++ podrum/protocol/mcbe/packet/move_player_packet.py
@@ -40,20 +40,27 @@ class move_player_packet(mcbe_packet):
     def decode_payload(self) -> None:
         self.runtime_entity_id: int = self.read_var_long()
         self.position: object = self.read_vector_3_float()
-        self.rotation: object = self.read_vector_3_float()
+        self.pitch: float = self.read_float_le()
+        self.yaw: float = self.read_float_le()
+        self.head_yaw: float = self.read_float_le()
         self.mode: int = self.read_unsigned_byte()
         self.on_ground: bool = self.read_bool()
-        if not self.feos():
-            self.riding_runtime_entity_id: int = self.read_float_le()
-            self.teleportation_cause: int = self.read_var_long()
-            self.entity_type: int = self.read_unsigned_byte()
+        self.riding_runtime_entity_id: int = self.read_var_int()
+        if self.mode == 2:
+            self.teleport_cause: int = self.read_int_le()
+            self.teleport_item: int = self.read_int_le()
+        self.tick: int = self.read_var_long()
  
     def encode_payload(self) -> None:
         self.write_var_long(self.runtime_entity_id)
         self.write_vector_3_float(self.position)
-        self.write_vector_3_float(self.rotation)
+        self.write_float_le(self.pitch)
+        self.write_float_le(self.yaw)
+        self.write_float_le(self.head_yaw)
         self.write_unsigned_byte(self.mode)
         self.write_bool(self.on_ground)
-        self.write_float_le(self.riding_runtime_entity_id)
-        self.write_var_long(self.teleportation_cause)
-        self.write_unsigned_byte(self.entity_type)
+        self.write_var_long(self.riding_runtime_entity_id)
+        if self.mode == 2:
+            self.write_int_le(self.teleport_cause)
+            self.write_int_le(self.teleport_item)
+        self.write_var_long(self.tick)
