c1c5f382f1d5da5e7de993f53de9aebd37b3895b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-10 17:44:12 +0300

    Create canonical_block_states_to_json.py

---
+++ canonical_block_states_to_json.py
@@ -0,0 +1,54 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from nbt_utils.utils.nbt_net_le_binary_stream import nbt_net_le_binary_stream
+import json
+
+file: object = open("canonical_block_states.nbt", "rb")
+stream: object = nbt_net_le_binary_stream(file.read())
+
+block_states: list = []
+
+while not stream.feos():
+    root_tag: object = stream.read_root_tag()
+    name: str = root_tag.get_tag("name").value
+    states: list = root_tag.get_tag("states").value
+    version: int = root_tag.get_tag("version").value
+    parsed_states: dict = {}
+    for state in states:
+        parsed_states[state.name] = state.value
+    block_states.append({
+        "name": name,
+        "states": parsed_states,
+        "version": version
+    })
+
+json.dump(block_states, open("canonical_block_states.json", "w"), indent = 4)
