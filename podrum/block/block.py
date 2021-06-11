a22b792e168425d293cbcceaeca0321e028762fb
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-11 19:11:32 +0300

    Update block.py

--- podrum/block/block.py
+++ podrum/block/block.py
@@ -1,52 +1,52 @@
 ################################################################################
- #                                                                              #
- #  ____           _                                                            #
- # |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
- # | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
- # |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
- # |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
- #                                                                              #
- # Copyright 2021 Podrum Studios                                                #
- #                                                                              #
- # Permission is hereby granted, free of charge, to any person                  #
- # obtaining a copy of this software and associated documentation               #
- # files (the "Software"), to deal in the Software without restriction,         #
- # including without limitation the rights to use, copy, modify, merge,         #
- # publish, distribute, sublicense, and/or sell copies of the Software,         #
- # and to permit persons to whom the Software is furnished to do so,            #
- # subject to the following conditions:                                         #
- #                                                                              #
- # The above copyright notice and this permission notice shall be included      #
- # in all copies or substantial portions of the Software.                       #
- #                                                                              #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
- # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
- # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
- # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
- # IN THE SOFTWARE.                                                             #
- #                                                                              #
- ################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
 
- from block.block_map import block_map
- from block.tool import tool
- from game_data.mcbe.item_id_map import item_id_map
+from block.block_map import block_map
+from block.tool import tool
+from game_data.mcbe.item_id_map import item_id_map
 
- class block:
-     def __init__(self, name: int, meta: int, hardness: float, blast_resistance: float) -> None:
-         self.name: str = name
-         self.meta: int = meta
-         self.hardness: float = hardness
-         self.blast_resistance: float = blast_resistance
-         self.runtime_id: int = block_map.get_runtime_id(name, meta)
-         self.network_id: int = item_id_map[name]
-         self.stack_size: int = 0
-         self.tool: int = tool.none
-         self.luminant: bool = False
-         self.transparent: bool = False
-         self.flammable: bool = False
-         self.catches_fire_from_lava: bool = False
-         self.creates_sources: bool = False
-         self.flow_distance: int = 0
-         self.flow_speed: int = 0
+class block:
+    def __init__(self, name: int, meta: int, hardness: float, blast_resistance: float) -> None:
+        self.name: str = name
+        self.meta: int = meta
+        self.hardness: float = hardness
+        self.blast_resistance: float = blast_resistance
+        self.runtime_id: int = block_map.get_runtime_id(name, meta)
+        self.network_id: int = item_id_map[name]
+        self.stack_size: int = 0
+        self.tool: int = tool.none
+        self.luminant: bool = False
+        self.transparent: bool = False
+        self.flammable: bool = False
+        self.catches_fire_from_lava: bool = False
+        self.creates_sources: bool = False
+        self.flow_distance: int = 0
+        self.flow_speed: int = 0
