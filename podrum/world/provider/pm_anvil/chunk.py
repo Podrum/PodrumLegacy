0b4b5b8d1c48a9410da066cf5728193955b25b0d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-11 13:55:20 +0300

    Update chunk.py

--- podrum/world/provider/pm_anvil/chunk.py
+++ podrum/world/provider/pm_anvil/chunk.py
@@ -54,3 +54,62 @@ class chunk(chunk):
         self.tile_entities: list = tile_entities
         self.light_populated: bool = False
         self.terrain_populated: bool = False
+            
+    def nbt_serialize(self) -> bytes:
+        stream: object = nbt_be_binary_stream()
+        self.recalculate_height_map()
+        sections: list = list_tag("Sections", [], tag_ids.compound_tag)
+        for i, sect in self.sections.items():
+            sections.value.append(compound_tag("", [
+                byte_array_tag("Blocks", sect.block_ids),
+                byte_array_tag("Data", sect.data_entries),
+                byte_array_tag("BlockLight", sect.block_light_entries),
+                byte_array_tag("SkyLight", sect.sky_light_entries)
+            ]))
+        root_tag: object = compound("", [
+            compound("Level", [
+                int_tag("xPos", self.x),
+                int_tag("zPos", self.z),
+                long_tag("LastUpdate", 0),
+                long_tag("InhabitedTime", 0),
+                byte_tag("TerrainPopulated", 1 if self.terrain_populated else 0),
+                byte_tag("LightPopulated", 1 if self.light_populated else 0),
+                sections,
+                byte_array_tag("Biomes", self.biomes),
+                list_tag("Entities", self.entities, tag_ids.compound_tag),
+                list_tag("TileEntities", self.tile_entities, tag_ids.compound_tag),
+                int_array_tag("HeightMap", self.height_map)
+            ]),
+            int_tag("DataVersion", 1343)
+        ])
+        stream.write_root_tag(root_tag)
+        return stream.data
+    
+    def nbt_deserialize(self, data: bytes) -> None:
+        stream = nbt_be_binary_stream(data)
+        root_tag: object = stream.read_root_tag()
+        if not isinstance(root_tag, compound_tag):
+            raise Exception("Invalid NBT data!")
+        if not root_tag.has_tag("Level"):
+            raise Exception("Level tag isnt present!")
+        level_tag: object = root_tag.get_tag("Level")
+        self.x: int = level_tag.get_tag("xPos").value
+        self.z: int = level_tag.get_tag("zPos").value
+        self.terrain_populated: bool = level_tag.get_tag("TerrainPopulated").value > 0
+        if level_tag.has_tag("LightPopulated"):
+            self.light_populated: bool = level_tag.get_tag("LightPopulated").value > 0
+        else:
+            self.light_populated: bool = False
+        sections_tag: object = level_tag.get_tag("Sections")
+        for section_tag in sections_tag.value:
+            self.sections[section_tag.get_tag("Y").value]: object = section(
+                section_tag.get_tag("Blocks").value,
+                section_tag.get_tag("Data").value,
+                section_tag.get_tag("BlockLight").value,
+                section_tag.get_tag("SkyLight").value
+            )
+        if level_tag.has_tag("Biomes"):
+            self.biomes: list = level_tag.get_tag("Biomes").value
+        self.entities: list = level_tag.get_tag("Entities").value
+        self.tile_entities: list = level_tag.get_tag("TileEntities").value
+        self.height_map: list = level_tag.get_tag("HeightMap").value
