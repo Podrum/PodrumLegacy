bce9bc5a6a49b366b3bab461f5afabd4dc4e386a
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-11 08:53:17 +0300

    Update and rename name_to_id.py to name_to_id_map.py

--- podrum/world/provider/anvil/name_to_id_map.py
+++ podrum/world/provider/anvil/name_to_id_map.py
@@ -29,7 +29,7 @@
 #                                                                              #
 ################################################################################
 
-name_to_id: dict = {
+name_to_id_map: dict = {
     "minecraft:air": 0,
     "minecraft:stone": 1,
     "minecraft:grass": 2,
