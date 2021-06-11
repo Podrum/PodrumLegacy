55c24373de2e1a07cf0df5ade480a63ba8dcc48f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-11 22:27:29 +0300

    Create generator_manager.py

---
+++ podrum/world/generator_manager.py
@@ -0,0 +1,45 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+class generator_manager:
+    def __init__(self):
+        self._generators: dict = {}
+          
+    def register_generator(self, generator: object) -> None:
+        self._generators[generator.generator_name]: object = generator
+          
+    def get_generator(self, generator_name: str) -> object:
+        if generator_name in self._generators:
+            return self._generators[generator_name]
+      
+    def remove_generator(self, generator_name: str) -> None:
+        if generator_name in self._generators:
+            del self._generators[generator_name]
