02524a3e3d0ead234b885799d35a30c786e7eed1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-11 10:40:15 +0300

    Update java_to_bedrock_map.py

--- podrum/world/provider/anvil/java_to_bedrock_map.py
+++ podrum/world/provider/anvil/java_to_bedrock_map.py
@@ -29,38 +29,7 @@
 #                                                                              #
 ################################################################################
 
-java_to_bedrock_map: dict = ################################################################################
-#                                                                              #
-#  ____           _                                                            #
-# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
-# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
-# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
-# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
-#                                                                              #
-# Copyright 2021 Podrum Studios                                                #
-#                                                                              #
-# Permission is hereby granted, free of charge, to any person                  #
-# obtaining a copy of this software and associated documentation               #
-# files (the "Software"), to deal in the Software without restriction,         #
-# including without limitation the rights to use, copy, modify, merge,         #
-# publish, distribute, sublicense, and/or sell copies of the Software,         #
-# and to permit persons to whom the Software is furnished to do so,            #
-# subject to the following conditions:                                         #
-#                                                                              #
-# The above copyright notice and this permission notice shall be included      #
-# in all copies or substantial portions of the Software.                       #
-#                                                                              #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
-# IN THE SOFTWARE.                                                             #
-#                                                                              #
-################################################################################
-
-name_to_id_map: dict = {
+java_to_bedrock_map: dict = {
     "minecraft:air 0": "minecraft:air 0",
     "minecraft:stone 0": "minecraft:stone 0",
     "minecraft:stone 1": "minecraft:stone 1",
