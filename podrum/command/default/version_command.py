7a8d75dc46d30b9c1ef1ea41a26d5c757f053409
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-13 19:09:13 +0300

    Update version_command.py

--- podrum/command/default/version_command.py
+++ podrum/command/default/version_command.py
@@ -32,10 +32,11 @@
 from version import version
 
 class version_command:
-    def __init__(self) -> None:
+    def __init__(self, server: object) -> None:
+        self.server: object = server
         self.name: str = "version"
         self.description: str = "version command"
         self.aliases: list = ["ver"]
     
-    def execute(self, args: list, sender: object, server: object) -> None:
+    def execute(self, args: list, sender: object) -> None:
         sender.send_message(f"This server is running Podrum version {version.podrum_version} {version.podrum_codename} on API {version.podrum_api_version}. This version is licensed under the {version.podrum_license} license.")
