4a9fbc7a8c5ff427e65c862acb3ec467ee823045
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-13 15:40:32 +0300

    Create metadata_flags_2_type.py

---
+++ podrum/protocol/mcbe/type/metadata_flags_2_type.py
@@ -0,0 +1,66 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+class metadata_flags_2_type:
+    flag_sneezing: 0,
+    flag_trusting: 1,
+    flag_rolling: 2,
+    flag_scared: 3,
+    flag_in_scaffolding: 4,
+    flag_over_scaffolding: 5,
+    flag_fall_through_scaffolding: 6,
+    flag_blocking: 7,
+    flag_transition_blocking: 8,
+    flag_blocked_using_shield: 9,
+    flag_blocked_using_damaged_shield: 10,
+    flag_sleeping: 11,
+    flag_wants_to_wake: 12,
+    flag_trade_interest: 13,
+    flag_door_breaker: 14,
+    flag_breaking_obstruction: 15,
+    flag_door_opener: 16,
+    flag_illager_captain: 17,
+    flag_stunned: 18,
+    flag_roaring: 19,
+    flag_delayed_attacking: 20,
+    flag_avoiding_mobs: 21,
+    flag_avoiding_block: 22,
+    flag_facing_target_to_range_attack: 23,
+    flag_hidden_when_invisible: 24,
+    flag_is_in_ui: 25,
+    flag_stalking: 26,
+    flag_emoting: 27,
+    flag_celebrating: 28,
+    flag_admiring: 29,
+    flag_celebrating_special: 30,
+    flag_unknown95: 31,
+    flag_ram_attack: 32,
+    flag_playing_dead: 33
