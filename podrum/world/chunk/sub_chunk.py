3cce63b12bd9dbbaf97b87c8460c02845656a567
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-13 16:25:11 +0300

    Update sub_chunk.py

--- podrum/world/chunk/sub_chunk.py
+++ podrum/world/chunk/sub_chunk.py
@@ -57,8 +57,10 @@ class sub_chunk:
     def get_highest_block_at(self, x: int, z: int, layer: int) -> int:
         return self.get_block_storage(layer).get_highest_block_at(x, z)
 
-    def network_serialize(self, stream: object) -> None:
+    def network_serialize(self) -> bytes:
+        stream: object = binary_stream()
         stream.write_unsigned_byte(8)
         stream.write_unsigned_byte(len(self.block_storages))
         for storage in self.block_storages.values():
             storage.network_serialize(stream)
+        return stream.data
