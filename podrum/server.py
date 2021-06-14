3d522f47e806f8773ca2f52ad791dbaf71db137d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-14 15:58:02 +0300

    Update server.py

--- podrum/server.py
+++ podrum/server.py
@@ -13,92 +13,26 @@
 #                                                       #
 #########################################################
 
-from block.block_manager import block_manager
 from block.block_map import block_map
-from block.default.air import air
-from block.default.andesite import andesite
-from block.default.andesite_smooth import andesite_smooth
-from block.default.bedrock import bedrock
-from block.default.cobblestone import cobblestone
-from block.default.diorite import diorite
-from block.default.diorite_smooth import diorite_smooth
-from block.default.dirt import dirt
-from block.default.granite import granite
-from block.default.granite_smooth import granite_smooth
-from block.default.grass import grass
-from block.default.stone import stone
 from command.command_interface import command_interface
-from command.command_manager import command_manager
-from command.default.help_command import help_command
-from command.default.plugins_command import plugins_command
-from command.default.reload_command import reload_command
-from command.default.say_command import say_command
-from command.default.stop_command import stop_command
-from command.default.version_command import version_command
 from config import config
 from console.logger import logger
-from event.event_manager import event_manager
+from managers import managers
 import os
-from plugin_manager import plugin_manager
 from protocol.mcbe.rak_net_interface import rak_net_interface
 import time
-from world.generator.flat import flat
-from world.generator.void import void
-from world.generator_manager import generator_manager
-from world.provider.anvil.anvil import anvil
-from world.provider.pm_anvil.pm_anvil import pm_anvil
-from world.provider_manager import provider_manager
-from world.world_manager import world_manager
 
 class server:
     def __init__(self) -> None:
         self.setup_config()
-        self.command_manager: object = command_manager()
+        block_map.load_map()
+        self.managers: object = managers(self)
         self.command_interface: object = command_interface(self)
-        self.event_manager: object = event_manager()
-        self.block_manager: object = block_manager()
-        self.provider_manager: object = provider_manager()
-        self.generator_manager: object = generator_manager()
-        self.world_manager: object = world_manager(self)
         self.rak_net_interface: object = rak_net_interface(self)
         self.logger: object = logger()
-        self.plugin_manager: object = plugin_manager(self)
         self.players: dict = {}
         self.current_entity_id: int = 1
         self.start()
-
-    def register_default_commands(self) -> None:
-        self.command_manager.register(help_command(self))
-        self.command_manager.register(plugins_command(self))
-        self.command_manager.register(reload_command(self))
-        self.command_manager.register(say_command(self))
-        self.command_manager.register(stop_command(self))
-        self.command_manager.register(version_command(self))
-        
-    def register_default_providers(self) -> None:
-        self.provider_manager.register_provider(anvil)
-        self.provider_manager.register_provider(pm_anvil)
-        
-    def register_default_generators(self) -> None:
-        self.generator_manager.register_generator(flat())
-        self.generator_manager.register_generator(void())
-        
-    def register_default_blocks(self) -> None:
-        self.block_manager.register_block(air())
-        self.block_manager.register_block(andesite())
-        self.block_manager.register_block(andesite_smooth())
-        self.block_manager.register_block(bedrock())
-        self.block_manager.register_block(cobblestone())
-        self.block_manager.register_block(diorite())
-        self.block_manager.register_block(diorite_smooth())
-        self.block_manager.register_block(dirt())
-        self.block_manager.register_block(granite())
-        self.block_manager.register_block(granite_smooth())
-        self.block_manager.register_block(grass())
-        self.block_manager.register_block(stone())
-        
-    def register_events(self) -> None:
-        pass
 
     def get_plugin_main(self, name):
         if name in self.plugin_manager.plugins:
@@ -131,21 +65,15 @@ class server:
     def start(self) -> None:
         start_time: float = time.time()
         self.logger.info("Podrum is starting up...")
-        block_map.load_map()
-        self.register_default_blocks()
-        self.register_default_generators()
         plugins_path: str = os.path.join(os.getcwd(), "plugins")
         if not os.path.isfile(plugins_path) and not os.path.isdir(plugins_path):
             os.mkdir(plugins_path)
-        self.plugin_manager.load_all(plugins_path)
-        self.register_default_providers()
+        self.managers.plugin_manager.load_all(plugins_path)
         worlds_path: str = os.path.join(os.getcwd(), "worlds")
         if not os.path.isfile(worlds_path) and not os.path.isdir(worlds_path):
             os.mkdir(worlds_path)
-        self.world_manager.load_world(self.config.data["world_name"])
-        self.world: object = self.world_manager.get_world_from_folder_name(self.config.data["world_name"])
-        self.register_default_commands()
-        self.register_events()
+        self.managers.world_manager.load_world(self.config.data["world_name"])
+        self.world: object = self.managers.world_manager.get_world_from_folder_name(self.config.data["world_name"])
         self.command_interface.start_interface()
         self.rak_net_interface.start_interface()
         finish_time: float = time.time()
@@ -155,8 +83,8 @@ class server:
     def stop(self) -> None:
         self.rak_net_interface.stop_interface()
         self.command_interface.stop_interface()
-        self.plugin_manager.unload_all()
-        self.world_manager.unload_all()
+        self.managers.plugin_manager.unload_all()
+        self.managers.world_manager.unload_all()
 
     def send_message(self, message: str) -> None:
         self.logger.info(message)
