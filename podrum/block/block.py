edafd12a16f9c6e619a77b04a24f1b8ca57c5fc8
Alexander Argentakis <alexandros.argentakis@gmail.com>
2021-06-14 14:19:06 +0300

    Replaced license.

--- podrum/block/block.py
+++ podrum/block/block.py
@@ -1,33 +1,17 @@
-################################################################################
-#                                                                              #
-#  ____           _                                                            #
-# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
-# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
-# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
-# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
-#                                                                              #
-# Copyright 2021 Podrum Studios                                                #
-#                                                                              #
-# Permission is hereby granted, free of charge, to any person                  #
-# obtaining a copy of this software and associated documentation               #
-# files (the "Software"), to deal in the Software without restriction,         #
-# including without limitation the rights to use, copy, modify, merge,         #
-# publish, distribute, sublicense, and/or sell copies of the Software,         #
-# and to permit persons to whom the Software is furnished to do so,            #
-# subject to the following conditions:                                         #
-#                                                                              #
-# The above copyright notice and this permission notice shall be included      #
-# in all copies or substantial portions of the Software.                       #
-#                                                                              #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
-# IN THE SOFTWARE.                                                             #
-#                                                                              #
-################################################################################
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
 
 from block.block_map import block_map
 from block.tool import tool
