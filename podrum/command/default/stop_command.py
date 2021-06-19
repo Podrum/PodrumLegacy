da76aed1887af023b7ca1146fd3bf9c50e31be47
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-19 17:56:24 +0300

    Update stop_command.py

--- podrum/command/default/stop_command.py
+++ podrum/command/default/stop_command.py
@@ -23,6 +23,6 @@ class stop_command:
     
     def execute(self, args: list, sender: object) -> None:
         sender.send_message("Stopping server...")
-        self.server.stop()
+        self.server.event_loop.create_task(self.server.stop())
         sender.send_message("Server stopped.")
         os.kill(os.getpid(), 15)
