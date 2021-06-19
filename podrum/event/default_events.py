d0fed34bb3c2908f3e8b6f126bbd1f83761d6012
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-19 15:23:03 +0300

    Create default_events.py

---
+++ podrum/event/default_events.py
@@ -0,0 +1,27 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+class default_events:
+    @staticmethod
+    def execute_command_event(user_input: str, sender: object, command_manager: object) -> None:
+        if len(user_input) > 0:
+            split_input: list = user_input.split()
+            command_name: str = split_input[0][1:]
+            command_args: list = split_input[1:]
+            commands: dict = command_manager.commands
+            if command_manager.has_command(command_name):
+                command_manager.execute(command_name, command_args, sender)
+            else:
+                sender.send_message("Invalid command!")
