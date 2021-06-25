5b407971fef53acc31d6cabf31a7f71924ad8aae
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-25 13:10:54 +0300

    Update server.py

--- podrum/server.py
+++ podrum/server.py
@@ -18,6 +18,7 @@ import os
 import platform
 from podrum.block.block_map import block_map
 from podrum.config import config
+from podrum.console_utils import console_utils
 from podrum.console.logger import logger
 from podrum.managers import managers
 from podrum.protocol.mcbe.rak_net_interface import rak_net_interface
@@ -100,37 +101,6 @@ class server:
         self.logger.info(message)
         
     async def console_input(self) -> None:
-        if platform.system() == "Windows":
-            import msvcrt
-        else:
-            import fcntl
-            fcntl.fcntl(sys.stdin, fcntl.F_SETFL, fcntl.fcntl(sys.stdin, fcntl.F_GETFL) | os.O_NONBLOCK)
-        result: str = ""
         while True:
-            if platform.system() == "Windows":
-                if msvcrt.kbhit():
-                    user_input: str = msvcrt.getch().decode()
-                    if user_input == "\b":
-                        result = result[0:-1]
-                    else:
-                        result += user_input
-                    if user_input == "\r":
-                        print("")
-                    else:
-                        print("\r\x1b[K" + result, end = "")
-                    if result.endswith("\r"):
-                        command: str = result[:-1]
-                        self.managers.event_manager.call_event("execute_command", command, self, self)
-                        result: str = ""
-                else:
-                    await asyncio.sleep(0.0001)
-            else:
-                user_input: str = sys.stdin.read(1)
-                if len(user_input) > 0:
-                    result += user_input
-                    if result.endswith("\n"):
-                        command: str = result[:-1]
-                        self.managers.event_manager.call_event("execute_command", command, self, self)
-                        result: str = ""
-                else:
-                    await asyncio.sleep(0.0001)
+            command: object = await console_utils.ainput()
+            self.managers.event_manager.call_event("execute_command", command.result(), self, self)
