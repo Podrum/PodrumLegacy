6345613984ea1746f5de36682cbfa79d7b59cbaf
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-25 13:06:44 +0300

    Update console_utils.py

--- podrum/console/console_utils.py
+++ podrum/console/console_utils.py
@@ -24,7 +24,7 @@ else:
 
 class console_utils:
     @staticmethod
-    async def ainput(self) -> None:
+    async def ainput() -> None:
         if platform.system() != "Windows":
             fcntl.fcntl(sys.stdin, fcntl.F_SETFL, fcntl.fcntl(sys.stdin, fcntl.F_GETFL) | os.O_NONBLOCK)
         out: object = asyncio.Future()
