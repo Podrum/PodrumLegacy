beecba7428907a063ea9e080abc74d349927a471
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-29 00:40:57 +0300

    Create recipes_type.py

---
+++ podrum/protocol/mcbe/type/recipes_type.py
@@ -0,0 +1,24 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+class recipes_type:
+    type_shapeless: int = 0
+    type_shaped: int = 1
+    type_furnace: int = 2
+    type_furnace_with_metadata: int = 3
+    type_multi: int = 4
+    type_shulker_box: int = 5
+    type_shapeless_chemistry: int = 6
+    type_shaped_chemistry: int = 7
