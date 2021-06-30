afd5b01eac5e6a7c53bdbddb12f5661d3e979271
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-06-30 17:20:03 +0300

    Create player_hotbar_packet.py

---
+++ podrum/protocol/mcbe/packet/player_hotbar_packet.py
@@ -0,0 +1,33 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+
+from podrum.protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
+from podrum.protocol.mcbe.packet.mcbe_packet import mcbe_packet
+
+class player_hotbar_packet(mcbe_packet):
+    def __init__(self, data: bytes = b"", pos: int = 0) -> None:
+        super().__init__(data, pos)
+        self.packet_id: int = mcbe_protocol_info.player_hotbar_packet
+        
+    def decode_payload(self) -> None:
+        self.selected_slot: int = self.read_var_int()
+        self.window_id: int = self.read_var_int()        
+        self.select_slot: dict = self.read_bool()
+        
+    def encode_payload(self) -> None:
+        self.write_var_int(self.selected_slot)
+        self.write_var_int(self.window_id)
+        self.write_bool(self.select_slot)
