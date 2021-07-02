49d30aafa018c1f976ecc03368fcdfb78e8b5043
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 10:25:43 +0300

    Create event.py

---
+++ podrum/event/event.py
@@ -0,0 +1,23 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.event.event_handler import event_handler
+
+class event:
+    canceled = False
+  
+    def call(self) -> None:
+        for listener in event_handler.get_listeners():
+            listener(self)
