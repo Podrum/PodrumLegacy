30f653aae0f31eecae520201989cf033f050e184
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 11:10:27 +0300

    Update managers.py

--- podrum/managers.py
+++ podrum/managers.py
@@ -101,6 +101,7 @@ class managers:
     def register_defaults(self) -> None:
         self.register_default_blocks()
         self.register_default_commands()
+        self.register_default_events()
         self.register_default_items()
         self.register_default_generators()
         self.register_default_providers()
