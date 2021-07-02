8e348fb1fd16aaced336b3101d22e66fc85dc124
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 09:13:54 +0300

    Update repeating_task.py

--- podrum/task/repeating_task.py
+++ podrum/task/repeating_task.py
@@ -20,22 +20,22 @@ class repeating_task(Thread):
     def __init__(self, task_object: object, args: list = [], interval: int = 0, interval_before: bool = False) -> None:
         super().__init__()
         self.setDaemon(True)
-        self.__task_object: object = task_object
-        self.__args: list = args
-        self.__interval: int = interval
-        self.__interval_before: bool = interval_before
+        self.task_object: object = task_object
+        self.args: list = args
+        self.interval: int = interval
+        self.interval_before: bool = interval_before
 
     def start(self) -> None:
-        self.__is_running: bool = True
+        self.is_running: bool = True
         super().start()
 
     def stop(self) -> None:
-        self.__is_running: bool = False
+        self.is_running: bool = False
 
     def run(self) -> None:
-        while self.__is_running:
-            if self.__interval_before:
-                sleep(self.__interval)
-            self.__task_object(*self.__args)
-            if not self.__interval_before:
-                sleep(self.__interval)
+        while self.is_running:
+            if self.interval_before:
+                sleep(self.interval)
+            self.task_object(*self.args)
+            if not self.interval_before:
+                sleep(self.interval)
