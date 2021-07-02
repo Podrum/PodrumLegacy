4fbf7bb13099504efafd3c7b86e119049d221119
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 09:22:23 +0300

    Update immediate_task.py

--- podrum/task/immediate_task.py
+++ podrum/task/immediate_task.py
@@ -20,14 +20,14 @@ class immediate_task(Thread):
     def __init__(self, task_object: object, args: list = [], interval: int = 0, interval_before: bool = False) -> None:
         super().__init__()
         self.setDaemon(True)
-        self.__task_object: object = task_object
-        self.__args: list = args
-        self.__interval: int = interval
-        self.__interval_before: bool = interval_before
+        self.task_object: object = task_object
+        self.args: list = args
+        self.interval: int = interval
+        self.interval_before: bool = interval_before
 
     def run(self) -> None:
-        if self.__interval_before:
-            sleep(self.__interval)
-        self.__task_object(*self.__args)
-        if not self.__interval_before:
-            sleep(self.__interval)
+        if self.interval_before:
+            sleep(self.interval)
+        self.task_object(*self.args)
+        if not self.interval_before:
+            sleep(self.interval)
