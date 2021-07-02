3298077bf6ef8435728f2055e77c38b8905c4d75
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 10:32:17 +0300

    Create player_join_event.py

---
+++ podrum/event/default/player/player_join_event.py
@@ -0,0 +1,20 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.event.event import event
+
+class player_join_event(event):
+    def __init__(self, player: object) -> None:
+        self.player: object = player
