a1494c697cd80e8e8dd30507e41919eaa69aaeef
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 10:44:29 +0300

    Update managers.py

--- podrum/managers.py
+++ podrum/managers.py
@@ -34,6 +34,8 @@ from podrum.command.default.reload_command import reload_command
 from podrum.command.default.say_command import say_command
 from podrum.command.default.stop_command import stop_command
 from podrum.command.default.version_command import version_command
+from podrum.event.default.player.player_join_event import player_join_event
+from podrum.event.event_manager import event_manager
 from podrum.item.default.stone import stone as stone_item
 from podrum.item.item_manager import item_manager
 from podrum.plugin_manager import plugin_manager
@@ -79,6 +81,9 @@ class managers:
         self.command_manager.register(say_command(self.server))
         self.command_manager.register(stop_command(self.server))
         self.command_manager.register(version_command(self.server))
+        
+    def register_default_events(self) -> None:
+        event_manager.register_event(player_join_event)
         
     def register_default_items(self) -> None:
         self.item_manager.register_item(stone_item())
