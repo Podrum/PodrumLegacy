57cc0511630fabbc19ca0d13fffa8c83115e587e
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-03 10:33:48 +0300

    Create player_action_packet.py

---
+++ podrum/protocol/mcbe/packet/player_action_packet.py
@@ -0,0 +1,35 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+
+from podrum.protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
+from podrum.protocol.mcbe.packet.mcbe_packet import mcbe_packet
+
+class player_action_packet(mcbe_packet):
+    def __init__(self, data: bytes = b"", pos: int = 0) -> None:
+        super().__init__(data, pos)
+        self.packet_id: int = mcbe_protocol_info.player_action_packet
+        
+    def decode_payload(self) -> None:
+        self.runtime_entity_id: int = self.read_var_long()
+        self.action: int = self.read_signed_var_int()
+        self.position: int = self.read_block_coordinates()
+        self.face: dict = self.read_signed_var_int()
+        
+    def encode_payload(self) -> None:
+        self.write_var_long(self.runtime_entity_id)
+        self.write_signed_var_int(self.action)
+        self.write_block_coordinates(self.position)
+        self.write_signed_var_int(self.face)
