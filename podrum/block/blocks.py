a8056917b2e9236be23d0220b7dbf7c41a8aa396
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-02 21:57:55 -0700

    Add files via upload

---
+++ podrum/block/blocks.py
@@ -0,0 +1,112 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.default.air import air
+from podrum.block.default.andesite import andesite
+from podrum.block.default.andesite_smooth import andesite_smooth
+from podrum.block.default.bedrock import bedrock
+from podrum.block.default.bone_block import bone_block
+from podrum.block.default.bookshelf import bookshelf
+from podrum.block.default.brewing_stand import brewing_stand
+from podrum.block.default.brown_mushroom import brown_mushroom
+from podrum.block.default.cactus import cactus
+from podrum.block.default.clay import clay
+from podrum.block.default.coal_block import coal_block
+from podrum.block.default.coal_ore import coal_ore
+from podrum.block.default.cobblestone import cobblestone
+from podrum.block.default.cobweb import cobweb
+from podrum.block.default.crafting_table import crafting_table
+from podrum.block.default.dandelion import dandelion
+from podrum.block.default.daylight_sensor import daylight_sensor
+from podrum.block.default.deadbush import deadbush
+from podrum.block.default.deepslate_coal_ore import deepslate_coal_ore
+from podrum.block.default.deepslate_diamond_ore import deepslate_diamond_ore
+from podrum.block.default.deepslate_emerald_ore import deepslate_emerald_ore
+from podrum.block.default.deepslate_gold_ore import deepslate_gold_ore
+from podrum.block.default.deepslate_iron_ore import deepslate_iron_ore
+from podrum.block.default.deepslate_lapis_ore import deepslate_lapis_ore
+from podrum.block.default.deepslate_redstone_ore import deepslate_redstone_ore
+from podrum.block.default.diamond_block import diamond_block
+from podrum.block.default.diamond_ore import diamond_ore
+from podrum.block.default.diorite import diorite
+from podrum.block.default.diorite_smooth import diorite_smooth
+from podrum.block.default.dirt import dirt
+from podrum.block.default.emerald_block import emerald_block
+from podrum.block.default.emerald_ore import emerald_ore
+from podrum.block.default.enchanting_table import enchanting_table
+from podrum.block.default.end_stone import end_stone
+from podrum.block.default.end_stone_brick import end_stone_brick
+from podrum.block.default.farmland import farmland
+from podrum.block.default.flower_pot import flower_pot
+from podrum.block.default.fire import fire
+from podrum.block.default.glass import glass
+from podrum.block.default.glass_pane import glass_pane
+from podrum.block.default.glowing_obsidian import glowing_obsidian
+from podrum.block.default.glowstone import glowstone
+from podrum.block.default.gold_block import gold_block
+from podrum.block.default.gold_ore import gold_ore
+from podrum.block.default.granite import granite
+from podrum.block.default.granite_smooth import granite_smooth
+from podrum.block.default.grass import grass
+from podrum.block.default.gravel import gravel
+from podrum.block.default.hardened_clay import hardened_clay
+from podrum.block.default.hay_bale import hay_bale
+from podrum.block.default.ice import ice
+from podrum.block.default.invisible_bedrock import invisible_bedrock
+from podrum.block.default.iron_bars import iron_bars
+from podrum.block.default.iron_block import iron_block
+from podrum.block.default.iron_ore import iron_ore
+from podrum.block.default.lapis_block import lapis_block
+from podrum.block.default.lapis_ore import lapis_ore
+from podrum.block.default.lava import lava
+from podrum.block.default.lit_pumpkin import lit_pumpkin
+from podrum.block.default.magma_block import magma
+from podrum.block.default.monster_spawner import monster_spawner
+from podrum.block.default.melon import melon
+from podrum.block.default.mossy_cobblestone import mossy_cobblestone
+from podrum.block.default.mycelium import mycelium
+from podrum.block.default.nether_brick_block import nether_brick_block
+from podrum.block.default.nether_quartz_ore import nether_quartz_ore
+from podrum.block.default.nether_wart_block import nether_wart_block
+from podrum.block.default.netherrack import netherrack
+from podrum.block.default.noteblock import noteblock
+from podrum.block.default.obsidian import obsidian
+from podrum.block.default.packed_ice import packed_ice
+from podrum.block.default.podzol import podzol
+from podrum.block.default.prismarine import prismarine
+from podrum.block.default.pumpkin import pumpkin
+from podrum.block.default.purpur import purpur
+from podrum.block.default.quartz_block import quartz_block
+from podrum.block.default.red_mushroom import red_mushroom
+from podrum.block.default.red_sandstone import red_sandstone
+from podrum.block.default.redstone_block import redstone_block
+from podrum.block.default.redstone_lamp import redstone_lamp
+from podrum.block.default.redstone_ore import redstone_ore
+from podrum.block.default.sand import sand
+from podrum.block.default.sandstone import sandstone
+from podrum.block.default.sea_lantern import sea_lantern
+from podrum.block.default.snow_block import snow_block
+from podrum.block.default.soul_sand import soul_sand
+from podrum.block.default.stone import stone
+from podrum.block.default.sponge import sponge
+from podrum.block.default.stone_bricks import stone_bricks
+from podrum.block.default.sugar_cane import sugar_cane
+from podrum.block.default.tnt import tnt
+from podrum.block.default.wooden_planks import wooden_planks
+from podrum.block.default.wool import wool
+
+class Blocks:
+    def __init__(self):
+        pass
