88ade52ae6e119432b031eed9a093ef46e51dcd8
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-03 14:28:41 +0300

    Create command_request_packet.py

---
+++ podrum/protocol/mcbe/packet/command_request_packet.py
@@ -0,0 +1,32 @@
+#########################################################                        
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.protocol.mcbe.mcbe_protocol_info import mcbe_protocol_info
+from podrum.protocol.mcbe.packet.mcbe_packet import mcbe_packet
+
+class command_request_packet(mcbe_packet):
+    def __init__(self, data: bytes = b"", pos: int = 0) -> None:
+        super().__init__(data, pos)
+        self.packet_id: int = mcbe_protocol_info.command_request_packet
+ 
+    def decode_payload(self) -> None:
+        self.command: str = self.read_string()
+        self.origin: int = self.read_var_int()
+        self.interval: int = self.read_bool()
+ 
+    def encode_payload(self) -> None:
+        self.write_string(self.command)
+        self.write_var_int(self.origin)
+        self.write_bool(self.interval)
