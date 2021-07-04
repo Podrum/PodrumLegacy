4acdbf2563d4dcb16935d37e5fb2219b82f75cf9
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-04 15:29:38 +0300

    Update sea_lantern.py

--- podrum/block/default/sea_lantern.py
+++ podrum/block/default/sea_lantern.py
@@ -18,7 +18,7 @@ from podrum.block.tool import tool
 
 class sea_lantern(block):
     def __init__(self) -> None:
-        super().__init__("minecraft:sealantern", 0, 0.3, 0.3)
+        super().__init__("minecraft:seaLantern", 0, 0.3, 0.3)
         self.stack_size: int = 64
         self.tool: int = tool.none
         self.luminant: bool = True
