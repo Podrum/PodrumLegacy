5494a3cf58809d8ad0b6ff8b98ba898cc50475f4
plun1331 <49261529+plun1331@users.noreply.github.com>
2021-07-03 22:30:59 -0700

    Update events.py

--- podrum/event/events.py
+++ podrum/event/events.py
@@ -19,3 +19,4 @@ from podrum.event.default.player.player_quit_event import player_quit_event
 from podrum.event.default.player.player_sneak_event import player_sneak_event
 from podrum.event.default.player.player_sprint_event import player_sprint_event
 from podrum.event.default.player.player_jump_event import player_jump_event
+from podrum.event.default.player.player_chat_event import player_chat_event
