37906d7260c66df7dae043451dc92ebf35189666
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-05 08:55:50 +0300

    Create window_type.py

---
+++ podrum/protocol/mcbe/type/window_type.py
@@ -0,0 +1,52 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+class window_type:
+    none: int = -9
+    inventory: int = -1
+    container: int = 0
+    workbench: int = 1
+    furnace: int = 2
+    enchantment: int = 3
+    brewing_stand: int = 4
+    anvil: int = 5
+    dispenser: int = 6
+    dropper: int = 7
+    hopper: int = 8
+    cauldron: int = 9
+    minecart_chest: int = 10
+    minecart_hopper: int = 11
+    horse: int = 12
+    beacon: int = 13
+    structure_editor: int = 14
+    trading: int = 15
+    command_block: int = 16
+    jukebox: int = 17
+    armor: int = 18
+    hand: int = 19
+    compound_creator: int = 20
+    element_constructor: int = 21
+    material_reducer: int = 22
+    lab_table: int = 23
+    loom: int = 24
+    lectern: int = 25
+    grindstone: int = 26
+    blast_furnace: int = 27
+    smoker: int = 28
+    stonecutter: int = 29
+    cartography: int = 30
+    hud: int = 31
+    jigsaw_editor: int = 32
+    smithing_table: int = 33
