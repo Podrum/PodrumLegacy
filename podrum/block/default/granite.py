a366ea70922ee604d62d1305238d3809aa156934
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-05 09:20:47 +0300

    Update granite.py

--- podrum/block/default/granite.py
+++ podrum/block/default/granite.py
@@ -13,9 +13,10 @@
 #                                                       #
 #########################################################
 
-from podrum.block.default.stone import stone
+from podrum.block.block import block
 
-class granite(stone):
+class granite(block):
     def __init__(self) -> None:
-        super().__init__()
-        self.meta: int = 1
+        super().__init__("minecraft:stone", 1, 1.5, 6)
+        self.stack_size: int = 64
+        self.tool: int = tool.pickaxe
