1a62990cb228d775d715771fbd2a2df7a0529aa1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-05 08:49:38 +0300

    Update window_id_type.py

--- podrum/protocol/mcbe/type/window_id_type.py
+++ podrum/protocol/mcbe/type/window_id_type.py
@@ -14,6 +14,24 @@
 #########################################################
 
 class window_id_type:
+    drop_contents: int = -100
+    beacon: int = -24
+    trading_output: int = -23
+    trading_use_inputs: int = -22
+    trading_input_2: int = -21
+    trading_input_1: int = -20
+    enchant_output: int = -17
+    enchant_material: int = -16
+    enchant_input: int = -15
+    anvil_output: int = -13
+    anvil_result: int = -12
+    anvil_material: int = -11
+    container_input: int = -10
+    crafting_use_ingredient: int = -5
+    crafting_result: int = -4
+    crafting_remove_ingredient: int = -3
+    crafting_add_ingredient: int = -2
+    none: int = -1
     inventory: int = 0
     first: int = 1
     last: int = 100
@@ -23,21 +41,3 @@ class window_id_type:
     hotbar: int = 122
     fixed_inventory: int = 123
     ui: int = 124
-    drop_contents: int = 156
-    beacon: int = 232
-    trading_output: int = 233
-    trading_use_inputs: int = 234
-    trading_input_2: int = 235
-    trading_input_1: int = 236
-    enchant_output: int = 239
-    enchant_material: int = 240
-    enchant_input: int = 241
-    anvil_output: int = 243
-    anvil_result: int = 244
-    anvil_material: int = 245
-    container_input: int = 246
-    crafting_use_ingredient: int = 251
-    crafting_result: int = 252
-    crafting_remove_ingredient: int = 253
-    crafting_add_ingredient: int = 254
-    none: int = 255
