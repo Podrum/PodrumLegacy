c6e6094b68abf78ea94b52f3519c7638b7494fa5
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-05 13:16:56 +0300

    Create interact_type.py

---
+++ podrum/protocol/mcbe/type/interact_type.py
@@ -0,0 +1,19 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+class interact_type:
+    leave_vehicle: int = 3
+    mouse_over_entity: int = 4
+    open_inventory: int = 4
