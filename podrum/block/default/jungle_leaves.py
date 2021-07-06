9c8cceea6bc5c6ae634a655325cc0465b5c07dd3
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 12:30:37 -0700

    Create jungle_leaves.py

---
+++ podrum/block/default/jungle_leaves.py
@@ -0,0 +1,27 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+from podrum.block.block_manager import block_manager
+
+class jungle_leaves(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:leaves", 3, 0.2, 0.2, "minecraft:leaves")
+        self.stack_size: int = 64
+        self.tool: int = tool.shears
+        self.transparent: bool = True
+        self.flammable: bool = True
+        self.catches_fire_from_lava = True
