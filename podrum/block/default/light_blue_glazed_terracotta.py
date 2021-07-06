8f7bab882207d21eb4416375481dc3f081481a25
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 10:37:46 -0700

    Create light_blue_glazed_terracotta.py

---
+++ podrum/block/default/light_blue_glazed_terracotta.py
@@ -0,0 +1,25 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+from podrum.block.block_manager import block_manager
+from podrum.block.default.colors import colors
+
+class light_blue_glazed_terracotta(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:light_blue_glazed_terracotta", 0, 1.4, 1.4)
+        self.stack_size: int = 64
+        self.tool: int = tool.pickaxe
