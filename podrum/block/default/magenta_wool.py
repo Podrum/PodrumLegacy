d46fd16ebc276718a82318f576e6e274a22706ca
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 09:39:59 -0700

    Create magenta_wool.py

---
+++ podrum/block/default/magenta_wool.py
@@ -0,0 +1,27 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+from podrum.block.block_manager import block_manager
+from podrum.block.default.colors import colors
+
+class magenta_wool(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:wool", 2, 0.8, 0.8)
+        self.stack_size: int = 64
+        self.tool: int = tool.shears
+        self.flammable: bool = True
+        self.catches_fire_from_lava: bool = True
