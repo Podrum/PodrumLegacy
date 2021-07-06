c768ed2d5695ce283b50acf5d84208512251d244
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-05 17:52:38 -0700

    Update stone.py

--- podrum/block/default/stone.py
+++ podrum/block/default/stone.py
@@ -17,14 +17,13 @@ from podrum.block.block import block
 from podrum.block.tool import tool
 from podrum.block.block_manager import block_manager
 
-types = {"stone": 0, "granite": 1, "polished_granite": 2, "diorite": 3, "polished_diorite": 4, "andesite": 5, "polished_andesite": 6}
-
 class stone(block):
     def __init__(self, meta="stone") -> None:
-        super().__init__("minecraft:stone", types[(str(meta).lower()).replace(" ", "_")] if isinstance(meta, str) else meta, 1.5, 6)
+        self.types = {"stone": 0, "granite": 1, "polished_granite": 2, "diorite": 3, "polished_diorite": 4, "andesite": 5, "polished_andesite": 6}
+        super().__init__("minecraft:stone", self.types[(str(meta).lower()).replace(" ", "_")] if isinstance(meta, str) else meta, 1.5, 6)
         self.stack_size: int = 64
         self.tool: int = tool.pickaxe
 
     def register(self) -> None:
-        for i in range(0, len(types)):
+        for i in range(0, len(self.types)):
             block_manager.register_block(block_manager(), stone(i))
