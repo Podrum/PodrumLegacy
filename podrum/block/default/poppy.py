4b6f20874d7fd7188a76503c0193e6db4750ed73
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 09:53:15 -0700

    Update poppy.py

--- podrum/block/default/poppy.py
+++ podrum/block/default/poppy.py
@@ -17,17 +17,11 @@ from podrum.block.block import block
 from podrum.block.tool import tool
 from podrum.block.block_manager import block_manager
 
-class red_flower(block):
-    def __init__(self, meta="poppy") -> None:
-        self.types = {"poppy": 0, "blue_orchid": 1, "allium": 2, "azure_bluet": 3, "red_tulip": 4, "orange_tulip": 5,
-                      "white_tulip": 6, "pink_tulip": 7, "oxeye_daisy": 8, "cornflower": 9, "lily_of_the_valley": 10}
-        super().__init__("minecraft:red_flower", self.types[meta] if isinstance(meta, str) else meta, 0, 0)
+class poppy(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:red_flower", 0, 0, 0)
         self.stack_size: int = 64
         self.tool: int = tool.none
         self.transparent: bool = True
         self.flammable: bool = True
         self.catches_fire_from_lava = True
-
-    def register(self) -> None:
-        for i in range(0, len(self.types)):
-            block_manager.register_block(block_manager(), red_flower(i))
