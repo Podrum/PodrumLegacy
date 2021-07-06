794112a08924a81748d1d0e709e742d88908064b
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 11:31:57 -0700

    Update black_concrete.py

--- podrum/block/default/black_concrete.py
+++ podrum/block/default/black_concrete.py
@@ -20,6 +20,6 @@ from podrum.block.default.colors import colors
 
 class black_concrete(block):
     def __init__(self) -> None:
-        super().__init__("minecraft:concrete", 15, 1.8, 1.8)
+        super().__init__("minecraft:concrete", 15, 1.8, 1.8, "minecraft:concrete")
         self.stack_size: int = 64
         self.tool: int = tool.pickaxe
