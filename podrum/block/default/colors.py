ed824ab93e725e902eb665221a797b025a7e2418
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 10:57:35 -0700

    Update colors.py

--- podrum/block/default/colors.py
+++ podrum/block/default/colors.py
@@ -18,3 +18,17 @@ class colors:
         color = (str(color).lower()).replace(" ", "_")
         self.types = {"white": 0, "orange": 1, "magenta": 2, "light_blue": 3, "yellow": 4, "lime": 5, "pink": 6, "gray": 7, "light_gray": 8, "cyan": 9, "purple": 10, "blue": 11, "brown": 12, "green": 13, "red": 14, "black": 15}
         self.color = self.types[color] if color in self.types else 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
