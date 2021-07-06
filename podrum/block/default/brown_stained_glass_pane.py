22dfd14883f1e3bebd02b315330cf25eab878434
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 11:40:52 -0700

    Create brown_stained_glass_pane.py

---
+++ podrum/block/default/brown_stained_glass_pane.py
@@ -0,0 +1,24 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+
+class brown_stained_glass_pane(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:stained_glass_pane", 12, 0.3, 0.3, "minecraft:stained_glass_pane")
+        self.stack_size: int = 64
+        self.tool: int = tool.none
+        self.transparent: bool = True
