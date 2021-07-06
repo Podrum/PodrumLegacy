0ea04dfcd01ecd3e890dd8ba2ffd399db4ccc622
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-06 10:54:51 -0700

    Update cyan_concrete_powder.py

--- podrum/block/default/cyan_concrete_powder.py
+++ podrum/block/default/cyan_concrete_powder.py
@@ -18,6 +18,6 @@ from podrum.block.tool import tool
 
 class cyan_concrete_powder(block):
     def __init__(self) -> None:
-        super().__init__("minecraft:concretePowder", 9, 0.5, 0.5)
+        super().__init__("minecraft:concretePowder", 9, 0.5, 0.5, "minecraft:concrete_powder")
         self.stack_size: int = 64
         self.tool: int = tool.shovel
