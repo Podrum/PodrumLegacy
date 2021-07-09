ab178382e052bbec217e544de26ea0580a5fe3b0
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-09 13:02:04 -0700

    Create acacia_wood_planks.py

---
+++ podrum/block/default/acacia_wood_planks.py
@@ -0,0 +1,27 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+from podrum.block.block_manager import block_manager
+from podrum.block.default.colors import colors
+
+class acacia_wood_planks(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:planks", 4, 3, 2, "minecraft:planks")
+        self.stack_size: int = 64
+        self.tool: int = tool.axe
+        self.flammable: bool = True
+        self.catches_fire_from_lava: bool = True
