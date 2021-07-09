9d63dc05e82e66c0cc1a15da739ac9add414e7b0
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-09 13:13:25 -0700

    Create still_lava.py

---
+++ podrum/block/default/still_lava.py
@@ -0,0 +1,23 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+
+class still_lava(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:lava", 0, -1, 100, "minecraft:lava")
+        self.tool: int = tool.none
+        self.luminant: int = 15
