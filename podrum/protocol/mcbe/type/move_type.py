8b1d98f1834c7719394883259c30922aa8226cb6
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-08 19:55:54 -0700

    Create move_type.py

---
+++ podrum/protocol/mcbe/type/move_type.py
@@ -0,0 +1,20 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+class move_type:
+    normal: int = 0
+    reset: int = 0
+    teleport: int = 0
+    rotation: int = 0
