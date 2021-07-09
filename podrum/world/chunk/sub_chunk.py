9acafd2594388e43b68b4e99aec597b729a52abd
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-10 00:56:54 +0300

    Update sub_chunk.py

--- podrum/world/chunk/sub_chunk.py
+++ podrum/world/chunk/sub_chunk.py
@@ -50,5 +50,5 @@ class sub_chunk:
     def network_serialize(self, stream: object) -> None:
         stream.write_unsigned_byte(8)
         stream.write_unsigned_byte(len(self.block_storages))
-        for storage in self.block_storages.values():
+        for storage in self.block_storages:
             storage.network_serialize(stream)
