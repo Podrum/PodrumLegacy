7b8911af1b002e81808bc8a6c253f526e8439f27
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-09 23:53:46 +0300

    Update sub_chunk.py

--- podrum/world/chunk/sub_chunk.py
+++ podrum/world/chunk/sub_chunk.py
@@ -21,22 +21,12 @@ class sub_chunk:
             self.block_storages: dict = {0: block_storage(), 1: block_storage()}
         else:
             self.block_storages: dict = block_storages
-                
-    def get_block_storage(self, layer: int) -> None:
-        if layer not in self.block_storages:
-            for i in range(0, layer + 1):
-                if i not in self.block_storages:
-                    self.block_storages[i] = block_storage()
-        return self.block_storages[layer]
-    
-    def is_empty(self) -> bool:
-        return bool(len(self.block_storages) == 0)
     
     def get_block_runtime_id(self, x: int, y: int, z: int, layer: int) -> int:
-        return self.get_block_storage(layer).get_block_runtime_id(x, y, z)
+        return self.block_storages[layer].get_block_runtime_id(x, y, z)
     
     def set_block_runtime_id(self, x: int, y: int, z: int, runtime_id: int, layer: int) -> None:
-        self.get_block_storage(layer).set_block_runtime_id(x, y, z, runtime_id)
+        self.block_storages[layer].set_block_runtime_id(x, y, z, runtime_id)
         
     def get_highest_block_at(self, x: int, z: int, layer: int) -> int:
         return self.get_block_storage(layer).get_highest_block_at(x, z)
