c99ba655e605b5e627730b89a8bc9afb769615af
AnticcX <45725948+AnticcX@users.noreply.github.com>
2021-07-09 13:43:38 -0700

    Create trapped_chest.py

---
+++ podrum/block/default/trapped_chest.py
@@ -0,0 +1,25 @@
+#########################################################
+#  ____           _                                     #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
+# |  __/ (_) | (_| | |  | |_| | | | | | |               #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
+#                                                       #
+# Copyright 2021 Podrum Team.                           #
+#                                                       #
+# This file is licensed under the GPL v2.0 license.     #
+# The license file is located in the root directory     #
+# of the source code. If not you may not use this file. #
+#                                                       #
+#########################################################
+
+from podrum.block.block import block
+from podrum.block.tool import tool
+
+class trapped_chest(block):
+    def __init__(self) -> None:
+        super().__init__("minecraft:trapped_chest", 0, 2.5, 2.5, "minecraft:trapped_chest")
+        self.stack_size: int = 64
+        self.tool: int = tool.axe
+        self.transparent: bool = True
+        self.catches_fire_from_lava: bool = True
