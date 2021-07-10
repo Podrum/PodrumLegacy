02275f900277230d5181f6f5ced0bcac5034b8c8
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-10 14:18:02 +0300

    Update geometry.py

--- podrum/geometry/geometry.py
+++ podrum/geometry/geometry.py
@@ -18,7 +18,7 @@ from podrum.geometry.vector_3 import vector_3
 
 class geometry:
     @staticmethod
-    def lerp_vector_3(self, a: object, b: object, t: float) -> object:
+    def lerp_vector_3(a: object, b: object, t: float) -> object:
         return vector_3(
             a.x + t * (b.x - a.x),
             a.y + t * (b.y - a.y),
@@ -26,7 +26,7 @@ class geometry:
         )
       
     @staticmethod
-    def lerp_vector_2(self, a: object, b: object, t: float) -> object:
+    def lerp_vector_2(a: object, b: object, t: float) -> object:
         return vector_2(
             a.x + t * (b.x - a.x),
             a.z + t * (b.z - a.z),
