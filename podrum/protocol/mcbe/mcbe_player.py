e3e093cd06181b135a04d22e6ca88fa19c9b4322
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-13 11:24:08 +0300

    Update mcbe_player.py

--- podrum/protocol/mcbe/mcbe_player.py
+++ podrum/protocol/mcbe/mcbe_player.py
@@ -143,11 +143,6 @@ class mcbe_player:
     def send_creative_content_packet(self) -> None:
         packet: object = packets.creative_content_packet()
         packet.entries = []
-        entry_id: int = 1
-        #for creative_item in creative_items:
-        item_obj: object = item("minecraft:planks", 3, 0)
-        packet.entries.append({"entry_id": entry_id, "item": item_obj.prepare_for_network()})
-        #entry_id += 1
         packet.encode()
         self.send_packet(packet.data)
              
