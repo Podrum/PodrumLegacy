b6df58a9a2b209ce5f5f2b8bf39ee0aec013e446
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-08-21 18:29:49 +0300

    Update block_manager.py

--- podrum/block/block_manager.py
+++ podrum/block/block_manager.py
@@ -13,12 +13,18 @@
 #                                                       #
 #########################################################
 
+from podrum.item.item import item
+
 class block_manager:
-    def __init__(self) -> None:
+    def __init__(self, server: object) -> None:
+        self.server: object = server
         self.blocks: dict = {}
 
     def register_block(self, block_obj: object) -> None:
         self.blocks[f"{block_obj.name} {block_obj.meta}"] = block_obj
+        item_obj: object = item(block_obj.item_name, block_obj.network_id, block_obj.meta)
+        item_obj.block_runtime_id = block_obj.runtime_id
+        self.server.managers.item_manager.register_item(item_obj)
         
     def remove_block(self, name: str, meta: int) -> None:
         if f"{name} {meta}" in self.blocks:
