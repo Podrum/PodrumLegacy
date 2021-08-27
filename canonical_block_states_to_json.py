9721ce73505170d196aabed7daef1f818f9850bc
Yohann Boniface <edhyjox@gmail.com>
2021-08-27 21:49:51 +0200

    ♻️ canonical_block_states_to_json.py

--- canonical_block_states_to_json.py
+++ canonical_block_states_to_json.py
@@ -15,27 +15,33 @@ r"""
 from nbt_utils.utils.nbt_net_le_binary_stream import nbt_net_le_binary_stream
 import json
 
-file: object = open("canonical_block_states.nbt", "rb")
-stream: object = nbt_net_le_binary_stream(file.read())
+with open("canonical_block_states.nbt", "rb") as f:
+    stream = nbt_net_le_binary_stream(f.read())
 
 block_states: list = []
 
 while not stream.feos():
-    root_tag: object = stream.read_root_tag()
+    root_tag = stream.read_root_tag()
+
     name: str = root_tag.get_tag("name").value
     states: list = root_tag.get_tag("states").value
     version: int = root_tag.get_tag("version").value
-    parsed_states: list = []
-    for state in states:
-        parsed_states.append({
+
+    parsed_states: list = [
+        {
             "name": state.name,
             "type": state.id,
             "value": state.value
-        })
-    block_states.append({
-        "name": name,
-        "states": parsed_states,
-        "version": version
-    })
+        } for state in states
+    ]
 
-json.dump(block_states, open("canonical_block_states.json", "w"), indent = 4)
+    block_states.append(
+        {
+            "name": name,
+            "states": parsed_states,
+            "version": version
+        }
+    )
+
+with open("canonical_block_states.json", "w") as f:
+    json.dump(block_states, f, indent=4)
